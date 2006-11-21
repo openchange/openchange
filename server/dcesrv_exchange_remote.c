ba03ac4eb60a7b06f3ba95b57a6284e5ef9856e2
Julien Kerihuel <j.kerihuel@openchange.org>
2006-11-21 22:40:18 +0000

    Add a dcerpc_server module in charge of the exchange interfaces registration. When this module is loaded prior the remote endpoint, it let us proxify the exchange_nsp and exchange_emsmdb ones.
    Change the exchange_nsp ncacn_np binding string to
    reflect how Exchange server is currently using it.
    
    Conform the IDL with latest pidl requirements and
    move the MAPI_DATA structure from exchange_nsp to
    exchange_emsmdb.
    
    jkerihuel

---
+++ server/dcesrv_exchange_remote.c
@@ -0,0 +1,44 @@
+/*
+   Exchange interface registration module
+
+   OpenChange Project
+
+   Copyright (C) Julien Kerihuel 2006
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+/*
+  This module registers the exchange interfaces prior loading the
+  dcesrv_remote endpoint.
+ */
+
+#include "openchange.h"
+#include "ndr_exchange.h"
+
+NTSTATUS librpc_register_interface(const struct dcerpc_interface_table *interface);
+
+NTSTATUS init_module(void)
+{
+	NTSTATUS status;
+
+	status = librpc_register_interface(&dcerpc_table_exchange_nsp);
+	if (NT_STATUS_IS_ERR(status)) return status;
+
+	status = librpc_register_interface(&dcerpc_table_exchange_emsmdb);
+	if (NT_STATUS_IS_ERR(status)) return status;
+
+	return NT_STATUS_OK;
+}

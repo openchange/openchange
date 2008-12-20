b4d78e19d69b259461c8b72b9dc99cfa331e3ca7
Brad Hards <bradh@openchange.org>
2008-12-20 06:20:53 +0000

    More apidox triviality.

--- libmapi/IStream.c
+++ libmapi/IStream.c
@@ -52,6 +52,7 @@
    \note Developers should call GetLastError() to retrieve the last
    MAPI error code. Possible MAPI error codes are:
    - MAPI_E_NOT_INITIALIZED: MAPI subsystem has not been initialized
+   - MAPI_E_INVALID_PARAMETER: A problem occured obtaining the session context
    - MAPI_E_CALL_FAILED: A network problem was encountered during the
      transaction
 

16b31840a8f78bc7eae21eece357e3a69f9911fd
Julien Kerihuel <j.kerihuel@openchange.org>
2009-03-12 12:51:14 +0000

    Make it possible to specify a version number for release different from major/minor from configure.ac

--- script/mkrelease.sh
+++ script/mkrelease.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 
 #
-# ./script/mkrelease.sh VERSION NICKNAME FIRST_REVISION
-# ./script/mkrelease.sh 0.7 PHASER 308
+# ./script/mkrelease.sh VERSION NICKNAME NICKNAME2 FIRST_REVISION
+# ./script/mkrelease.sh 0.7 PHASER Phaser 308
 #
 
 TMPDIR=`mktemp libmapi-XXXXX`
 rm $TMPDIR || exit 1
 svn export . $TMPDIR || exit 1
-svn log -r$3:HEAD > $TMPDIR/CHANGELOG || exit 1
+svn log -r$4:HEAD > $TMPDIR/CHANGELOG || exit 1
 
 ( cd $TMPDIR/
  ./autogen.sh || exit 1
  ./configure || exit 1
  make || exit 1
  sed -i "s/^OPENCHANGE_VERSION_IS_SVN_SNAPSHOT=yes/OPENCHANGE_VERSION_IS_SVN_SNAPSHOT=no/g" VERSION || exit 1
+ sed -i "s/^OPENCHANGE_VERSION_RELEASE_NICKNAME=.*/OPENCHANGE_VERSION_RELEASE_NICKNAME=$3/g" VERSION || exit 1
+ sed -i "s/^OPENCHANGE_VERSION_RELEASE_NUMBER=.*/OPENCHANGE_VERSION_RELEASE_NUMBER=$1/g" VERSION || exit 1
+
  ./autogen.sh || exit 1
  ./configure || exit 1
  make doxygen || exit 1

e0d1062fdbcb4a87202054b172174083fd128c30
Brad Hards <bradh@openchange.org>
2010-10-06 10:19:27 +0000

    Remove bunding stuff, which just causes more breakage.
    According to tridge, waf should do the right thing by default.

--- script/installsamba4.sh
+++ script/installsamba4.sh
@@ -288,7 +288,7 @@ compile() {
     ./autogen.sh
     error_check $? "samba4 autogen"
 
-    ./configure.developer --bundled-libraries=ldb,subunit,NONE --prefix=$SAMBA_PREFIX
+    ./configure.developer --prefix=$SAMBA_PREFIX
     error_check $? "samba4 configure"
 
     echo "Step2: Compile Samba4 (Source)"

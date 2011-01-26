1ef9e61c7a01e9846cc6d1becc9e691a4980321e
Julien Kerihuel <j.kerihuel@openchange.org>
2011-01-26 22:16:51 +0000

    Introduce debugging macros and levels for backend willing to use a consistent debugging mechanism

--- mapiproxy/libmapistore/mapistore_common.h
+++ mapiproxy/libmapistore/mapistore_common.h
@@ -34,6 +34,43 @@
 
 #include <ldb.h>
 
+#define	MSTORE_LEVEL_CRITICAL		0
+#define	MSTORE_LEVEL_HIGH		1
+#define	MSTORE_LEVEL_MEDIUM		2
+#define	MSTORE_LEVEL_NORMAL		3
+#define	MSTORE_LEVEL_LOW		4
+#define	MSTORE_LEVEL_INFO		5
+#define	MSTORE_LEVEL_DEBUG		6
+#define	MSTORE_LEVEL_PEDANTIC		7
+#define	MSTORE_LEVEL_EXTREME		8
+
+#define	MSTORE_DEBUG_FMT_SUCCESS	"* [%s:%d][%s]: "
+#define	MSTORE_DEBUG_FMT_INFO		"I [%s:%d][%s]: "
+#define	MSTORE_DEBUG_FMT_ERROR		"! [%s:%d][%s]: "
+#define	MSTORE_DEBUG_FMT_PARAM		__FILE__, __LINE__, __FUNCTION__
+
+#define	__MSTORE_DEBUG(l, mstore_msg, msg)						\
+	if (msg) {									\
+		DEBUG(l, (mstore_msg "%s", MSTORE_DEBUG_FMT_PARAM, msg));		\
+	} else {									\
+		DEBUG(l, (mstore_msg, MSTORE_DEBUG_FMT_PARAM));				\
+	}										
+
+#define	__MSTORE_DEBUG_FMT(l, mstore_msg, fmt, msg)					\
+	if (msg && fmt) {								\
+		DEBUG(l, (mstore_msg fmt, MSTORE_DEBUG_FMT_PARAM, msg));		\
+	} else {									\
+		DEBUG(l, (mstore_msg, MSTORE_DEBUG_FMT_PARAM));				\
+	}
+
+#define	MSTORE_DEBUG_INFO(l,msg)		__MSTORE_DEBUG(l, MSTORE_DEBUG_FMT_INFO, msg)
+#define	MSTORE_DEBUG_SUCCESS(l,msg)		__MSTORE_DEBUG(l, MSTORE_DEBUG_FMT_SUCCESS, msg)
+#define	MSTORE_DEBUG_ERROR(l,msg)		__MSTORE_DEBUG(l, MSTORE_DEBUG_FMT_ERROR, msg)
+
+#define	MSTORE_DEBUG_SUCCESS_FMT(l,fmt,msg)	__MSTORE_DEBUG_FMT(l, MSTORE_DEBUG_FMT_SUCCESS, fmt, msg)
+#define	MSTORE_DEBUG_INFO_FMT(l,fmt,msg)	__MSTORE_DEBUG_FMT(l, MSTORE_DEBUG_FMT_INFO, fmt, msg)
+#define	MSTORE_DEBUG_ERROR_FMT(l,fmt,msg)	__MSTORE_DEBUG_FMT(l, MSTORE_DEBUG_FMT_ERROR, fmt, msg)
+
 struct mapistore_backend_context;
 
 __BEGIN_DECLS

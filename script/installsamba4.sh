6bcab7b98db29c492beb9c1df650cec22f9bef1f
Brad Hards <bradh@openchange.org>
2011-03-03 23:07:44 +0000

    Add workaround for missing samba4 headers
    Workaround (but not fix) for bug 350.

--- script/installsamba4.sh
+++ script/installsamba4.sh
@@ -235,6 +235,11 @@ patch() {
 	    ;;
     esac
 
+    echo "[+] Patching Samba build system for ugly header installation hack (http://tracker.openchange.org/issues/350)"
+    sed -i "s/deps='talloc ndr NDR_SECURITY'/deps='talloc ndr NDR_SECURITY',\\
+                  public_headers='dom_sid.h secace.h secacl.h security_descriptor.h security_token.h security.h session.h sddl.h privileges.h access_check.h',\\
+                  header_path='samba\/security'/g" samba4/libcli/security/wscript_build
+
     return $?
 }
 

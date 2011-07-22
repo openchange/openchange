309ecc1e44e3856f83d906801f17c678ba84250e
Wolfgang Wourdeau <wsourdeau@inverse.ca>
2011-07-22 13:53:46 +0000

    Moved the GLOBSetRunner class into its own module

--- testprogs/parse-syncbuffer.py
+++ testprogs/parse-syncbuffer.py
@@ -27,6 +27,8 @@ import struct
 import sys
 import time
 
+from globset import GLOBSetRunner
+
 PARSER_START, PARSER_PRERESPONSE, PARSER_RESPONSE, PARSER_DONE = range(4)
 
 MarkerTags = { 0x40090003: "PidTagStartTopFld",
@@ -57,6 +59,11 @@ ICSStateProperties = { 0x40170003: "PidTagIdsetGiven",
                        0x67960102: "PidTagCnsetSeen",
                        0x67da0102: "PidTagCnsetSeenFAI",
                        0x67d20102: "PidTagCnsetRead"  }
+DeltaStateProperties = { 0x67e50102: "PidTagIdsetDeleted",
+                         0x40210102: "PidTagIdsetNoLongerInScope",
+                         0x67930102: "PidTagIdsetExpired",
+                         0x402d0102: "PidTagIdsetRead",
+                         0x402e0102: "PidTagIdsetUnread" }
 PropFile = "/home/wsourdeau/src/openchange/libmapi/conf/mapi-properties"
 Properties = None
 PropertyTypes = { 0x0000: "PT_UNSPECIFIED",
@@ -98,135 +105,6 @@ def ReadProperties(filename):
                 props[propTag] = propName
 
     return props
-
-class GLOBSetRunner:
-    def __init__(self, buffer, pos):
-        self.buffer = buffer
-        self.start_pos = pos
-        self.pos = pos
-        self.end = False
-        self.stack = []
-        self.stackByteLength = 0
-        self.ranges = []
-
-    def _doPush1(self):
-        self._doPush(1)
-
-    def _doPush2(self):
-        self._doPush(2)
-
-    def _doPush3(self):
-        self._doPush(3)
-
-    def _doPush4(self):
-        self._doPush(4)
-
-    def _doPush5(self):
-        self._doPush(5)
-
-    def _doPush6(self):
-        self._doPush(6)
-
-    def _doPush(self, nbr):
-        bytes = self.buffer[self.pos:self.pos+nbr]
-        # print "push %d bytes: (%s)" % (nbr, ", ".join(["0x%.2x" % ord(x) for x in bytes]))
-        self.stack.append(bytes)
-        self.stackByteLength = self.stackByteLength + nbr
-        self.pos = self.pos + nbr
-        if self.stackByteLength == 6:
-            # print "  buffer filled"
-            self._doRange()
-            self._doPop()
-
-    def _doPop(self):
-        bytes = self.stack.pop()
-        nbr = len(bytes)
-        # print "popped %d bytes" % nbr
-        self.stackByteLength = self.stackByteLength - nbr
-
-    def _doBitmask(self):
-        startValue = ord(self.buffer[self.pos])
-        mask = ord(self.buffer[self.pos+1])
-        self.pos = self.pos + 2
-
-        combined = self._rangeValue("".join(self.stack)) << 8
-
-        blank = False
-        lowValue = startValue + 1
-        highValue = startValue + 1
-        for x in xrange(8):
-            bit = 1 << x
-            if blank:
-                if (mask & bit) != 0:
-                    blank = False
-                    lowValue = startValue + 1 + x
-                    highValue = lowValue
-            else:
-                if (mask & bit) == 0 or x == 7:
-                    self.ranges.append((combined | lowValue, combined | highValue))
-                    blank = True
-                else:
-                    highValue = startValue + 1 + x
-
-    def _doRange(self):
-        nbr = 6 - self.stackByteLength
-        
-        combined = "".join(self.stack)
-        if nbr > 0:
-            # print "pair covering %d bytes" % nbr
-            lowValue = self._rangeValue(combined
-                                        + self.buffer[self.pos:self.pos+nbr])
-            self.pos = self.pos + nbr
-            highValue = self._rangeValue(combined
-                                        + self.buffer[self.pos:self.pos+nbr])
-            self.pos = self.pos + nbr
-        elif nbr == 0:
-            # print "singleton (implied)"
-            lowValue = self._rangeValue(combined)
-            highValue = lowValue
-        else:
-            raise Exception, "reached negative range count"
-
-        self.ranges.append((lowValue, highValue))
-
-    def _rangeValue(self, bytes):
-        value = 0
-
-        base = 1
-        for x in bytes:
-            value = value + ord(x) * base
-            base = base * 256
-
-        return value
-
-    def _doEnd(self):
-        # print "end reached"
-        self.end = True
-        if len(self.stack) > 0:
-            raise Exception, "stack should be empty"
-
-    def run(self):
-        command_methods = { 0x01: self._doPush1,
-                            0x02: self._doPush2,
-                            0x03: self._doPush3,
-                            0x04: self._doPush4,
-                            0x05: self._doPush5,
-                            0x06: self._doPush6,
-                            0x50: self._doPop,
-                            0x42: self._doBitmask,
-                            0x52: self._doRange,
-                            0x00: self._doEnd }
-
-        while not self.end:
-            command = ord(self.buffer[self.pos])
-            self.pos = self.pos + 1
-            if command_methods.has_key(command):
-                command_method = command_methods[command]
-                command_method()
-            else:
-                raise Exception, "unknown command: %x" % command
-
-        return (self.pos - self.start_pos)
 
 class SyncBufferParser:
     def __init__(self, lines, pos = 0):
@@ -318,6 +196,10 @@ class SyncBufferPrinter:
         elif ICSStateProperties.has_key(tag):
             property_name = ICSStateProperties[tag]
             print "ICS %.8x (%s):" % (tag, property_name),
+            consumed = self._printIDSet(pos)
+        elif DeltaStateProperties.has_key(tag):
+            property_name = DeltaStateProperties[tag]
+            print "Delta %.8x (%s):" % (tag, property_name),
             consumed = self._printIDSet(pos)
         else:
             colType = tag & 0x0fff

a9e1fb602e904bac214581a21850e0cc2c87ae75
Wolfgang Wourdeau <wsourdeau@inverse.ca>
2011-10-17 13:15:07 +0000

    Fixed a useless talloc_reference, part of a leak of backend objects that are released only when closing the emsmdb session

--- mapiproxy/servers/default/emsmdb/emsmdbp_object.c
+++ mapiproxy/servers/default/emsmdb/emsmdbp_object.c
@@ -327,7 +327,7 @@ _PUBLIC_ struct emsmdbp_object *emsmdbp_object_open_folder(TALLOC_CTX *mem_ctx, 
 				mapistore_indexing_record_add_fid(emsmdbp_ctx->mstore_ctx, contextID, fid);
 			}
 			folder_object->object.folder->contextID = contextID;
-			(void) talloc_reference(folder_object, folder_object->backend_object);
+			/* (void) talloc_reference(folder_object, folder_object->backend_object); */
 		}
 		else {
 			DEBUG(0, ("%s: opening openchangedb folder\n", __FUNCTION__));

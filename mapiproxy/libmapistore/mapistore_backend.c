32e659f1fca05aec6c3758ee73daa4ecba40c3d2
Wolfgang Wourdeau <wsourdeau@inverse.ca>
2012-01-31 19:05:35 +0000

    Reverted hackish fix to reenable the freeing of backend resources

--- mapiproxy/libmapistore/mapistore_backend.c
+++ mapiproxy/libmapistore/mapistore_backend.c
@@ -399,7 +399,7 @@ _PUBLIC_ enum mapistore_error mapistore_backend_add_ref_count(struct backend_con
  */
 _PUBLIC_ enum mapistore_error mapistore_backend_delete_context(struct backend_context *bctx)
 {
-	if (bctx->ref_count > 1) {
+	if (bctx->ref_count) {
 		bctx->ref_count -= 1;
 		return MAPISTORE_ERR_REF_COUNT;
 	}

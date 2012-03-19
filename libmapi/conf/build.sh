79cbce6c543d89dad7f94d8230549d2e6bc84aee
Matthieu Patou <mat@matws.net>
2012-03-19 19:10:10 +0000

    Create setup/mapistore if not existing

--- libmapi/conf/build.sh
+++ libmapi/conf/build.sh
@@ -3,4 +3,5 @@
 ./libmapi/conf/mparse.pl --parser=mapicodes --outputdir=libmapi/ libmapi/conf/mapi-codes
 ./libmapi/conf/mparse.pl --parser=codepage_lcid --outputdir=libmapi/ libmapi/conf/codepage-lcid
 ./libmapi/conf/mparse.pl --parser=mapistore_nameid --outputdir=mapiproxy/libmapistore/ libmapi/conf/mapi-named-properties
+[ ! -d "setup/mapistore" ] && mkdir --parent "setup/mapistore"
 ./libmapi/conf/mparse.pl --parser=mapistore_namedprops --outputdir=setup/mapistore/ libmapi/conf/mapi-named-properties

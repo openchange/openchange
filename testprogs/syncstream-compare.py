bb375d981a387b28e9e5be0fe0ce8022dcde5dfe
Wolfgang Wourdeau <wsourdeau@inverse.ca>
2012-07-25 01:42:12 +0000

    Added copyright notice

--- testprogs/syncstream-compare.py
+++ testprogs/syncstream-compare.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python
+#
+# syncstream-compare.py
+#
+# Copyright (C) Wolfgang Sourdeau 2012
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#   
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#   
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # This script compare two chunks of the output from parse-syncbuffer and
 # report the differences on stdout, in order to help in property-related

b84f99c64e751182fa4b8e2e8774a796077d48ce
Samuel Cabrero <scabrero@zentyal.com>
2013-05-24 20:42:55 +0200

    Add new parameters to mapi profile for RPC over HTTP
    --roh               Use RPC over HTTP (default TLS and proxy port to 443)
    --roh-no-tls        Do not use TLS (change default proxy port to 80)
    --roh-proxy-port    Set RPC proxy port to use
    
    To tell the dcerpc library if TLS must be used or not, the tls flag
    is added to the binding string.
    
    Signed-off-by: Julien Kerihuel <j.kerihuel@openchange.org>

--- libmapi/mapi_profile.h
+++ libmapi/mapi_profile.h
@@ -8,12 +8,12 @@
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -55,6 +55,8 @@ struct mapi_profile
 	uint32_t	exchange_version;
 	const char	*kerberos;
 	bool		roh;
+	bool		roh_tls;
+	uint32_t	roh_proxy_port;
 };
 
 typedef int (*mapi_profile_callback_t)(struct PropertyRowSet_r *, const void *);

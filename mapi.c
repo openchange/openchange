dc26f7a2dcf32613053d2c08b2b79b1609a830ff
Javier Amor García <jamor@zentyal.com>
2013-07-13 14:44:21 +0200

    cleanup, add example.php

--- mapi.c
+++ mapi.c
@@ -49,40 +49,6 @@ PHP_MSHUTDOWN(mapi)
 }
 */
 
-static char * mapiprofile_list(struct mapi_context *mapi_ctx, const char *profdb)
-{
-	enum MAPISTATUS		retval;
-	struct SRowSet		proftable;
-	uint32_t		count = 0;
-
-	memset(&proftable, 0, sizeof (struct SRowSet));
-	if ((retval = GetProfileTable(mapi_ctx, &proftable)) != MAPI_E_SUCCESS) {
-		mapi_errstr("GetProfileTable", retval);
-		exit (1);
-	}
-
-
-	printf("We have %u profiles in the database:\n", proftable.cRows);
-
-
-
-	for (count = 0; count != proftable.cRows; count++) {
-		const char	*name = NULL;
-		uint32_t	dflt = 0;
-
-		name = proftable.aRow[count].lpProps[0].value.lpszA;
-		dflt = proftable.aRow[count].lpProps[1].value.l;
-
-		if (dflt) {
-			printf("\tProfile = %s [default]\n", name);
-		} else {
-			printf("\tProfile = %s\n", name);
-		}
-
-	}
-}
-
-
 
 
 PHP_FUNCTION(print_profiles)
@@ -123,65 +89,6 @@ PHP_FUNCTION(print_profiles)
 		}
 
 	}
-}
-
-static void mapiprofile_dump(struct mapi_context *mapi_ctx, const char *profdb, const char *opt_profname)
-{
-	TALLOC_CTX		*mem_ctx;
-	enum MAPISTATUS		retval;
-	struct mapi_profile	*profile;
-	char			*profname;
-	char			*exchange_version = NULL;
-
-	mem_ctx = talloc_named(mapi_ctx->mem_ctx, 0, "mapiprofile_dump");
-	profile = talloc(mem_ctx, struct mapi_profile);
-
-	if (!opt_profname) {
-		if ((retval = GetDefaultProfile(mapi_ctx, &profname)) != MAPI_E_SUCCESS) {
-			mapi_errstr("GetDefaultProfile", retval);
-			talloc_free(mem_ctx);
-			exit (1);
-		}
-	} else {
-		profname = talloc_strdup(mem_ctx, (const char *)opt_profname);
-	}
-
-	retval = OpenProfile(mapi_ctx, profile, profname, NULL);
-	talloc_free(profname);
-
-	if (retval && (retval != MAPI_E_INVALID_PARAMETER)) {
-		talloc_free(mem_ctx);
-		mapi_errstr("OpenProfile", retval);
-		exit (1);
-	}
-
-	switch (profile->exchange_version) {
-	case 0x0:
-		exchange_version = talloc_strdup(mem_ctx, "exchange 2000");
-		break;
-	case 0x1:
-		exchange_version = talloc_strdup(mem_ctx, "exchange 2003/2007");
-		break;
-	case 0x2:
-		exchange_version = talloc_strdup(mem_ctx, "exchange 2010");
-		break;
-	default:
-		printf("Error: unknown Exchange server\n");
-		goto end;
-	}
-
-	printf("Profile: %s\n", profile->profname);
-	printf("\texchange server == %s\n", exchange_version);
-	printf("\tencryption      == %s\n", (profile->seal == true) ? "yes" : "no");
-	printf("\tusername        == %s\n", profile->username);
-	printf("\tpassword        == %s\n", profile->password);
-	printf("\tmailbox         == %s\n", profile->mailbox);
-	printf("\tworkstation     == %s\n", profile->workstation);
-	printf("\tdomain          == %s\n", profile->domain);
-	printf("\tserver          == %s\n", profile->server);
-
-end:
-	talloc_free(mem_ctx);
 }
 
 PHP_FUNCTION(dump_profile)

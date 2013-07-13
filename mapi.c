de7d24c97e6c506c319867c02ef3dd553330a05b
Javier Amor García <jamor@zentyal.com>
2013-07-13 15:12:06 +0200

    added initlaize_mapi helper function

--- mapi.c
+++ mapi.c
@@ -50,118 +50,111 @@ PHP_MSHUTDOWN(mapi)
 */
 
 
+struct mapi_context* initialize_mapi()
+{
+  char *profdb = "/home/jag/.openchange/profiles.ldb";
+  struct mapi_context   *mapi_ctx;
+  enum MAPISTATUS        retval;
+  if (retval != MAPI_E_SUCCESS) {
+    char *err_str = mapi_get_errstr(retval);
+    php_error(E_ERROR, err_str);
+    // TODO BAIL OUT
+
+  }
+  return mapi_ctx;
+}
 
 PHP_FUNCTION(print_profiles)
 {
-  struct mapi_context	*mapi_ctx;	/*!< mapi context */
-  char *profdb = "/home/jag/.openchange/profiles.ldb";
-  enum MAPISTATUS retval;
-  retval = MAPIInitialize(&mapi_ctx, profdb);
-  if (retval != MAPI_E_SUCCESS) {
-    mapi_errstr("MAPIInitialize", retval);
-    RETURN_STRING("MAPI FAILED XXX", 1);
-  }
+  struct mapi_context *mapi_ctx = initialize_mapi();
 
   struct SRowSet proftable;
-  uint32_t count = 0;
-
   memset(&proftable, 0, sizeof (struct SRowSet));
+  enum MAPISTATUS               retval;
+  if ((retval = GetProfileTable(mapi_ctx, &proftable)) != MAPI_E_SUCCESS) {
+    mapi_errstr("GetProfileTable", retval);
+    exit (1);
+  }
 
-	if ((retval = GetProfileTable(mapi_ctx, &proftable)) != MAPI_E_SUCCESS) {
-		mapi_errstr("GetProfileTable", retval);
-		exit (1);
-	}
+  php_printf("We have %u profiles in the database:\n", proftable.cRows);
 
+  uint32_t count;
+  for (count = 0; count != proftable.cRows; count++) {
+    const char      *name = NULL;
+    uint32_t        dflt = 0;
 
-	php_printf("We have %u profiles in the database:\n", proftable.cRows);
+    name = proftable.aRow[count].lpProps[0].value.lpszA;
+    dflt = proftable.aRow[count].lpProps[1].value.l;
 
-	for (count = 0; count != proftable.cRows; count++) {
-		const char	*name = NULL;
-		uint32_t	dflt = 0;
+    if (dflt) {
+      php_printf("\tProfile = %s [default]\n", name);
+    } else {
+      php_printf("\tProfile = %s\n", name);
+    }
 
-		name = proftable.aRow[count].lpProps[0].value.lpszA;
-		dflt = proftable.aRow[count].lpProps[1].value.l;
-
-		if (dflt) {
-			php_printf("\tProfile = %s [default]\n", name);
-		} else {
-			php_printf("\tProfile = %s\n", name);
-		}
-
-	}
+  }
 }
 
 PHP_FUNCTION(dump_profile)
 {
-  // initialize and hardocde-values
-        char* opt_profname  = NULL; // dumnp default by now
-  struct mapi_context	*mapi_ctx;	/*!< mapi context */
-  char *profdb = "/home/jag/.openchange/profiles.ldb";
-  enum MAPISTATUS retval;
-  retval = MAPIInitialize(&mapi_ctx, profdb);
-  if (retval != MAPI_E_SUCCESS) {
-    mapi_errstr("MAPIInitialize", retval);
-    RETURN_STRING("MAPI FAILED XXX", 1);
-  }
-  // end
+    struct mapi_context *mapi_ctx = initialize_mapi();
 
-	TALLOC_CTX		*mem_ctx;
+    char * opt_profname = NULL; //DEFAULT FOR NOW
+    TALLOC_CTX              *mem_ctx;
+    enum MAPISTATUS         retval;
+    struct mapi_profile     *profile;
+    char                    *profname;
+    char                    *exchange_version = NULL;
 
+    mem_ctx = talloc_named(mapi_ctx->mem_ctx, 0, "mapiprofile_dump");
+    profile = talloc(mem_ctx, struct mapi_profile);
 
-        //	enum MAPISTATUS		retval; //declarated in provisional section
-	struct mapi_profile	*profile;
-	char			*profname;
-	char			*exchange_version = NULL;
+    if (!opt_profname) {
+      if ((retval = GetDefaultProfile(mapi_ctx, &profname)) != MAPI_E_SUCCESS) {
+        mapi_errstr("GetDefaultProfile", retval);
+        talloc_free(mem_ctx);
+        exit (1);
+      }
+    } else {
+      profname = talloc_strdup(mem_ctx, (const char *)opt_profname);
+    }
 
-	mem_ctx = talloc_named(mapi_ctx->mem_ctx, 0, "mapiprofile_dump");
-	profile = talloc(mem_ctx, struct mapi_profile);
+    retval = OpenProfile(mapi_ctx, profile, profname, NULL);
+    talloc_free(profname);
 
-	if (!opt_profname) {
-		if ((retval = GetDefaultProfile(mapi_ctx, &profname)) != MAPI_E_SUCCESS) {
-			mapi_errstr("GetDefaultProfile", retval);
-			talloc_free(mem_ctx);
-			exit (1);
-		}
-	} else {
-		profname = talloc_strdup(mem_ctx, (const char *)opt_profname);
-	}
+    if (retval && (retval != MAPI_E_INVALID_PARAMETER)) {
+      talloc_free(mem_ctx);
+      mapi_errstr("OpenProfile", retval);
+      exit (1);
+    }
 
-	retval = OpenProfile(mapi_ctx, profile, profname, NULL);
-	talloc_free(profname);
+    switch (profile->exchange_version) {
+    case 0x0:
+      exchange_version = talloc_strdup(mem_ctx, "exchange 2000");
+      break;
+    case 0x1:
+      exchange_version = talloc_strdup(mem_ctx, "exchange 2003/2007");
+      break;
+    case 0x2:
+      exchange_version = talloc_strdup(mem_ctx, "exchange 2010");
+      break;
+    default:
+      php_printf("Error: unknown Exchange server\n");
+      goto end;
+    }
 
-	if (retval && (retval != MAPI_E_INVALID_PARAMETER)) {
-		talloc_free(mem_ctx);
-		mapi_errstr("OpenProfile", retval);
-		exit (1);
-	}
+    php_printf("Profile: %s\n", profile->profname);
+    php_printf("\texchange server == %s\n", exchange_version);
+    php_printf("\tencryption      == %s\n", (profile->seal == true) ? "yes" : "no");
+    php_printf("\tusername        == %s\n", profile->username);
+    php_printf("\tpassword        == %s\n", profile->password);
+    php_printf("\tmailbox         == %s\n", profile->mailbox);
+    php_printf("\tworkstation     == %s\n", profile->workstation);
+    php_printf("\tdomain          == %s\n", profile->domain);
+    php_printf("\tserver          == %s\n", profile->server);
 
-	switch (profile->exchange_version) {
-	case 0x0:
-		exchange_version = talloc_strdup(mem_ctx, "exchange 2000");
-		break;
-	case 0x1:
-		exchange_version = talloc_strdup(mem_ctx, "exchange 2003/2007");
-		break;
-	case 0x2:
-		exchange_version = talloc_strdup(mem_ctx, "exchange 2010");
-		break;
-	default:
-		php_printf("Error: unknown Exchange server\n");
-		goto end;
-	}
-
-	php_printf("Profile: %s\n", profile->profname);
-	php_printf("\texchange server == %s\n", exchange_version);
-	php_printf("\tencryption      == %s\n", (profile->seal == true) ? "yes" : "no");
-	php_printf("\tusername        == %s\n", profile->username);
-	php_printf("\tpassword        == %s\n", profile->password);
-	php_printf("\tmailbox         == %s\n", profile->mailbox);
-	php_printf("\tworkstation     == %s\n", profile->workstation);
-	php_printf("\tdomain          == %s\n", profile->domain);
-	php_printf("\tserver          == %s\n", profile->server);
-
-end:
-	talloc_free(mem_ctx);
+ end:
+    talloc_free(mem_ctx);
 
 
 }
@@ -170,10 +163,10 @@ end:
 
 PHP_FUNCTION(hello_mapi)
 {
-	enum MAPISTATUS		retval;
-  TALLOC_CTX		*mem_ctx;
-  struct mapitest		mt;
-   const char		*opt_profdb = NULL;
+        enum MAPISTATUS         retval;
+  TALLOC_CTX            *mem_ctx;
+  struct mapitest               mt;
+   const char           *opt_profdb = NULL;
    // Initialize MAPI subsystem *\/ */
    if (!opt_profdb) {
      opt_profdb = talloc_asprintf(mem_ctx, DEFAULT_PROFDB, getenv("HOME"));

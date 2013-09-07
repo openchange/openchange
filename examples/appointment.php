a517f4dbf5ed92ec74963ebd5f268f4189b8fe22
Javier Amor García <jamor@zentyal.com>
2013-09-07 11:33:31 +0200

    moved exampels php to its own dir

---
+++ examples/appointment.php
@@ -0,0 +1,28 @@
+<?php
+$mapi = new MAPIProfileDB("/home/jag/.openchange/profiles.ldb");
+echo "MAPI DB path: '", $mapi->path(), "'\n";
+
+
+echo "Profile test\n";
+$mapiProfile = $mapi->getProfile('test');
+
+
+echo "Logon test profile\n";
+$session = $mapiProfile->logon();
+
+echo "Get mailbox\n";
+
+$mailbox = $session->mailbox();
+echo "Mailbox name "  . $mailbox->getName() . "\n";
+
+
+$calendar = $mailbox->calendar();
+echo "Calendar folder " . $calendar->getName() . "/" . $calendar->getFolderType() .  "\n";
+
+$id = "0x1307130000000001";
+$message = $calendar->openMessage($id);
+print "Message\n";
+var_dump($message);
+
+
+?>
---
+++ examples/simple-hierarchy.php
@@ -0,0 +1,24 @@
+<?php
+echo "Create ProfileDB\n";
+$mapi = new MAPIProfileDB("/home/jag/.openchange/profiles.ldb");
+
+echo "Get default profile\n";
+$mapiProfile = $mapi->getProfile();
+
+#echo "Logon default profile\n";
+#$session = $mapiProfile->logon();
+#
+#echo "Get mailbox\n";
+#$mailbox = $session->mailbox();
+#echo "Mailbox name "  . $mailbox->getName() . "\n";
+#
+#echo "Get CONTACTS folder\n\n";
+#$contacts = $mailbox->contacts();
+#echo "contacts->getID -> " . $contacts->getID() . "\n";
+#
+#echo "Get message\n";
+#$messageId = '0xA4010E0000000001';
+#$message = $contacts->openMessage($messageId);
+#echo var_dump($message->FileUnder);
+#
+?>

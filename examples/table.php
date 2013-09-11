d847ffca6a0bd4fd2bbb50c2bcc96ea47a9dc8c6
Javier Amor García <jamor@zentyal.com>
2013-09-11 19:47:36 +0200

    table.php example

---
+++ examples/table.php
@@ -0,0 +1,38 @@
+<?php
+$mapi = new MAPIProfileDB("/home/jag/.openchange/profiles.ldb");
+echo "MAPI DB path: '", $mapi->path(), "'\n";
+
+echo "Default profile\n";
+$mapiProfile = $mapi->getProfile();
+
+echo "Logon default profile\n";
+$session = $mapiProfile->logon();
+
+echo "Get mailbox\n";
+$mailbox = $session->mailbox();
+#var_dump($mailbox);
+echo "Mailbox name "  . $mailbox->getName() . "\n";
+
+echo "CONTACTS\n\n";
+$contacts = $mailbox->contacts();
+echo "Contacts item type " . $contacts->getFolderType() . "\n";
+
+#echo "Contacts getMessageTable \n";
+#$table0 =  $contacts->getMessageTable();
+#echo "Get all messages\n";
+#var_dump($table0->getMessages());
+#echo "END end of messages\n";
+
+echo "Contacts getMessageTable with properties PidLidFileUnder, PidLidEmail1EmailAddress \n";
+$table2 =  $contacts->getMessageTable(PidLidFileUnder, PidLidEmail1EmailAddress);
+
+echo "Get 4 summary contacts\n";
+var_dump($table2->summary(4));
+
+echo "Get 1 summary contact\n";
+var_dump($table2->summary(1));
+
+echo "Get remaining summary contact\n";
+var_dump($table2->summary());
+
+?>

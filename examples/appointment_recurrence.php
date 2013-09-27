54a7133c80c42054a486ff8d20aa21f92bfdac2f
Javier Amor García <jamor@zentyal.com>
2013-09-27 12:36:34 +0200

    appointmt recurrence script

--- examples/appointment_recurrence.php
+++ examples/appointment_recurrence.php
@@ -1,47 +1,28 @@
 <?php
-$dbPath = "/home/jag/.openchange/profiles.ldb";
-$profileName = 'u2';
-$id = "0x2500000000000001";
+# System dependent varialbes:
+$path = "/home/jag/.openchange/profiles.ldb";
+$profileName = 'miky';
+$eventId = "0x3A08010000000001";
+
+# END system dependent variables
 
 
-$mapi = new MAPIProfileDB($dbPath);
-echo "MAPI DB path: '", $mapi->path(), "'\n";
 
 
-
-echo "Profile test\n";
+$mapi = new MAPIProfileDB($path);
+echo "=> MAPI Profile Database Path: '", $mapi->path(), "'\n";
 $mapiProfile = $mapi->getProfile($profileName);
-
-
-echo "Logon test profile\n";
 $session = $mapiProfile->logon();
-
-echo "Get mailbox\n";
-
 $mailbox = $session->mailbox();
-echo "Mailbox name "  . $mailbox->getName() . "\n";
 
 
+$inbox = $mailbox->inbox();
+echo "=> Opening calensdar Folder\n";
 $calendar = $mailbox->calendar();
-echo "Calendar folder " . $calendar->getName() . "/" . $calendar->getFolderType() .  "\n";
 
+$event = $calendar->openMessage($eventId);
+$recur = $event->getRecurrence(PidLidAppointmentRecur);
 
-$message = $calendar->openMessage($id, 1);
+var_dump($recur);
 
-#$toSet = array('StartTimeOffset' => 0);
-$toSet = array(
-		   'RecurrencePattern' => array(),
-                   'StartTimeOffset' => 4,
-                   'EndTimeOffset' => 64
-         );
-#$mapi->debug(true, 10);
-$message->setRecurrence($toSet);
-
-
-$message->save();
-unset($message);
-
-$message = $calendar->openMessage($id, 1);
-$rec = $message->getRecurrence();
-var_dump($rec);
 ?>

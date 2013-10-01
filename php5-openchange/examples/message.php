be5c23f9e37f807d8427cd3561af780470a7cbe7
Javier Amor García <jamor@zentyal.com>
2013-10-01 17:53:09 +0200

    converted example to test

--- php5-openchange/examples/message.php
+++ php5-openchange/examples/message.php
@@ -1,4 +1,6 @@
 <?php
+include('./test-helpers.php');
+
 # System dependent varialbes:
 $path = "/home/jag/.openchange/profiles.ldb";
 $profileName = 'u2';
@@ -6,112 +8,80 @@ $contactMessageId = '0x7500000000000001';
 $roContactId = $contactMessageId;
 # END system dependent variables
 
-
-function dumpContact($contact) {
-  echo "Contact ID " . $contact->getID() . "\n";
-  echo "PidLidEmail1EmailAddress: " . $contact->get(PidLidEmail1EmailAddress) . "\n";
-  echo "PidTagEmailAddress: " . $contact->get(PidTagEmailAddress) . "\n";
-  echo "PidTagCompanyName: " . $contact->get(PidTagCompanyName) . "\n";
-  echo "PidTagDisplayName: " . $contact->get(PidTagDisplayName) . "\n";
-  echo "PidTagGivenName: " . $contact->get(PidTagGivenName) . "\n";
-  echo "PidLidBusyStatus: " . $contact->get(PidLidBusyStatus) . "\n";
-  echo "PidLidWorkAddressStreet: " .  $contact->get(PidLidWorkAddressStreet) . "\n";
-
-  echo "\n";
-}
-
+$pidTagBody1 = 'body1';
+$pidTagDisplayName1 = 'displayName1';
+$pidLidWorkAddressStreet1 = 'street1';
+$pidLidEmail1EmailAddress1 = 'jk1@example1.org';
+$pidTagBody2 = 'body2';
+$pidTagDisplayName2 = 'displayName2';
+$pidLidWorkAddressStreet2 = 'street2';
+$pidLidEmail1EmailAddress2 = 'jk2@example2.org';
 
 $mapi = new MAPIProfileDB($path);
-echo "=> MAPI Profile Database Path: '", $mapi->path(), "'\n";
+ok($mapi, "MAPIProfileDB opened from $path");
+
 $mapiProfile = $mapi->getProfile($profileName);
+ok($mapiProfile, "Profile $profileName");
+
 $session = $mapiProfile->logon();
+ok($session, "Logon with $profileName");
+
 $mailbox = $session->mailbox();
+ok($mailbox, "Default mailbox for $profileName");
 
-
-$inbox = $mailbox->inbox();
-echo "Inbox item type " . $inbox->getFolderType() . "\n";
-
-echo "=> Opening Contact Folder\n";
 $contacts = $mailbox->contacts();
-
-
-echo "Open RO message and show its properties\n";
-$roContact = $contacts->openMessage($contactMessageId, MAPIMessage::RO);
-dumpContact($roContact);
-$roContact->dump();
+ok($contacts, "Opening contact folder");
 
 $message = $contacts->openMessage($contactMessageId,  MAPIMessage::RW);
-echo "=> Message with message ID " . $message->getID() . " opened\n";
-echo "Body content format: " . $message->getBodyContentFormat() . "\n";
+ok($message, "Open message RW");
+is($message->getID(), $contactMessageId, "Checking opening message ID");
+is($message->getBodyContentFormat(), "txt", "Checking body content format");
 
-echo "Contact just after oepning it\n";
-dumpContact($message);
+diag("Setting multiple messages properties to first set");
+$message->set(
+	PidTagBody, $pidTagBody1,
+	PidLidWorkAddressStreet, $pidLidWorkAddressStreet1,
+	PidLidEmail1EmailAddress, $pidLidEmail1EmailAddress1
+);
 
-echo "Check one-argument prop set: setting PidTagDisplayName to 'juju' and PidTagDescription to 'descChanged'\n";
-$message->set(PidTagDisplayName, 'juju');
-$message->set(PidTagBody, 'descChanged');
-echo "Set PidLidBusyStatus to 1\n";
-$message->set(PidLidBusyStatus, 1);
+diag("Setting one property PidTagDisplayName to first set");
+$message->set(PidTagDisplayName, $pidTagDisplayName1);
 
-print "[1] PidLidWorkAddressStreet: " . $message->get(PidLidWorkAddressStreet);
-$message->set(PidLidWorkAddressStreet, "New Before street");
-
-echo "Set PidLidContactCharacterSet to 33";
-$message->set(PidLidContactCharacterSet, 33);
-
-$email = $message->get(PidLidEmail1EmailAddress);
-if ($email == "changed@a.org") {
-   echo "=> [1] Set PidLidEmail1EmailAddress to jkerihuel@zentyal.com\n";
-   $message->set(PidLidEmail1EmailAddress, "jkeriheuel@zentyal.com", PidTagDisplayName, 'julien');
-} else if ($email == "jkerihuel@zentyal.com") {
-   echo "=> [2] Set PidLidEmail1EmailAddress to changed@a.org\n";
-   $message->set(PidLidEmail1EmailAddress, "changed@a.org", PidTagDisplayName, 'changedD');
-} else {
-  echo "Not expected value\n";
-   $message->set(PidLidEmail1EmailAddress, "jkerihuel@zentyal.com", PidTagDisplayName, 'julien');
-}
-
-
-echo "=> SaveChangesMessage \n";
 $message->save();
 unset($message);
 
-echo "===================================================\n";
+diag ("- Saving changes");
+$message = $contacts->openMessage($contactMessageId, MAPIMessage::RW);
+ok($message, "Reopening message in RW mode");
 
+is($message->get(PidTagDisplayName), $pidTagDisplayName1, "Checking PidTagDisplayName1 has changed properly");
+$multipleValues = $message->get(PidTagBody, PidLidWorkAddressStreet, PidLidEmail1EmailAddress);
+is($multipleValues[PidTagBody], $pidTagBody1, "Checking value as PidTagBody (retrieved in a multiple value get)");
+is($multipleValues[PidLidWorkAddressStreet], $pidLidWorkAddressStreet1, "Checking value as PidLidWorkAddressStreet (retrieved in a multiple value get)");
+is($multipleValues[PidLidEmail1EmailAddress], $pidLidEmail1EmailAddress1, "Checking value as PidLidEmail1EmailAddress (retrieved in a multiple value get)");
 
+diag("Setting values again");
+$message->set(
+	PidTagBody , $pidTagBody2,
+	PidLidWorkAddressStreet , $pidLidWorkAddressStreet2,
+	PidLidEmail1EmailAddress , $pidLidEmail1EmailAddress2
+);
+$message->set(PidTagDisplayName, $pidTagDisplayName2);
 
-$message = $contacts->openMessage($contactMessageId, 1);
-echo "Dump after reopening after save\n";
-dumpContact($message);
-echo "=> Message with message ID " . $message->getID() . " opened\n";
-
-echo "Set PidLidBusyStatus to 2\n";
-$message->set(PidLidBusyStatus, 2);
-
-print "[2] PidLidWorkAddressStreet: " . $message->get(PidLidWorkAddressStreet);
-$message->set(PidLidWorkAddressStreet, "New After street");
-
-echo "Reverting changes\n";
-if ($email == "changed@a.org") {
-   echo "=> [1] Set PidLidEmail1EmailAddress to jkerihuel@zentyal.com\n";
-   $message->set(PidLidEmail1EmailAddress, "jkerihuel@zentyal.com", PidTagDisplayName, 'julien');
-} else if ($email == "jkerihuel@zentyal.com") {
-   echo "=> [2] Set PidLidEmail1EmailAddress to changed@a.org\n";
-   $message->set(PidLidEmail1EmailAddress, "changed@a.org", PidTagDisplayName, 'changedD');
-} else {
-  echo "Not expected value\n";
-   $message->set(PidLidEmail1EmailAddress, "jkerihuel@zentyal.com", PidTagDisplayName, 'julien');
-}
-echo "Setting contact link to 14\n";
-$message->set(PidLidContactCharacterSet, 14);
-
-
-echo "=> SaveChangesMessage \n";
 $message->save();
+unset($message);
 
-echo "Dump after second save\n";
-print "PidLidWorkAddressStreet: " . $message->get(PidLidWorkAddressStreet);
-dumpContact($message);
+diag("- Saving last changes");
+
+$message = $contacts->openMessage($contactMessageId, MAPIMessage::RO);
+ok($message, "Reopening message in RO mode");
+
+is($message->get(PidTagDisplayName), $pidTagDisplayName2, "Checking PidTagDisplayName2 has changed properly");
+$multipleValues = $message->get(PidTagBody, PidLidWorkAddressStreet, PidLidEmail1EmailAddress);
+is($multipleValues[PidTagBody], $pidTagBody2, "Checking value as PidTagBody (retrieved in a multiple value get)");
+is($multipleValues[PidLidWorkAddressStreet], $pidLidWorkAddressStreet2, "Checking value as PidLidWorkAddressStreet (retrieved in a multiple value get)");
+is($multipleValues[PidLidEmail1EmailAddress], $pidLidEmail1EmailAddress2, "Checking value as PidLidEmail2EmailAddress (retrieved in a multiple value get)");
+
 
 unset($message);
 

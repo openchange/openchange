27e888c96b39b038f0bb205a7b089f9ddf97ab0e
Javier Amor García <jamor@zentyal.com>
2013-10-01 18:13:06 +0200

    converted to test

--- php5-openchange/examples/new_message.php
+++ php5-openchange/examples/new_message.php
@@ -1,4 +1,5 @@
 <?php
+include('./test-helpers.php');
 # System dependent varialbes:
 $path = "/home/jag/.openchange/profiles.ldb";
 $profileName = 'u2';
@@ -7,50 +8,44 @@ $contactMessageId = '0xA4010E0000000001';
 
 
 $mapi = new MAPIProfileDB($path);
-echo "=> MAPI Profile Database Path: '", $mapi->path(), "'\n";
+ok($mapi, "Opened MAPIProfile with path $path");
 $mapiProfile = $mapi->getProfile($profileName);
+ok($mapiProfile, "Get profile $profileName");
 $session = $mapiProfile->logon();
+ok($session, "Logon with profile $profileName");
 $mailbox = $session->mailbox();
-
-
-$inbox = $mailbox->inbox();
-echo "Inbox item type " . $inbox->getFolderType() . "\n";
-
-echo "=> Opening Contact Folder\n";
+ok($mailbox, "Default mailbox");
 $contacts = $mailbox->contacts();
+ok($contacts, "Open contacts folder");
 
 $ids = array();
 
-echo "Create new contact\n";
 $newContact = $contacts->createMessage(PidLidEmail1EmailAddress, 'first_new@contacts.org', PidTagCompanyName, 'to_delete_org', PidTagDisplayName, 'first');
+ok($newContact, "Created new contact");
+is(get_class($newContact), "MAPIContact", "Check returned new class type");
 $ids[] = $newContact->getID();
-var_dump($newContact);
-print "New contact ID " . $newContact->getID() . "\n";
 unset($newContact);
 
-echo "Create a second contact\n";
 $newContact = $contacts->createMessage(PidLidEmail1EmailAddress, 'second_new@contacts.org', PidTagCompanyName, 'to_delete_org', PidTagDisplayName, 'second');
+ok($newContact, "Created a second new contact");
+is(get_class($newContact), "MAPIContact", "Check returned new class type for second new contact");
 $ids[] = $newContact->getID();
-var_dump($newContact);
-print "New contact ID " . $newContact->getID() . "\n";
 unset($newContact);
 
-echo "Opening created messages\n";
 foreach ($ids as $newId) {
     $retrieved = $contacts->openMessage($newId);
-    var_dump($retrieved);
+    ok($retrieved, "Check that we can retrieve a new created contact by its ID=$newId");
+    is(get_class($retrieved), "MAPIContact", "Check class type for retrieved contact");
+    is($retrieved->getID(), $newId, "Check ID for retrieved contact");
     unset($retrieved);
 }
 
-echo "Delete created messages\n";
+diag ("Delete created contacts");
 call_user_func_array(array($contacts, 'deleteMessages'), $ids);
 
-
-echo "Tried to open removed messages\n";
 foreach ($ids as $newId) {
     $retrieved = $contacts->openMessage($newId);
-    var_dump($retrieved);
-    unset($retrieved);
+    ok(is_null($retrieved), "Check that removed contact $newId has been removed");
 }
 
 

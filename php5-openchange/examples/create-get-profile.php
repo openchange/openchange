0af11c66d5348d015b417fa96f97d4b2d90dba2b
Javier Amor García <jamor@zentyal.com>
2013-10-01 16:29:00 +0200

    using tests on create-get-profile

--- php5-openchange/examples/create-get-profile.php
+++ php5-openchange/examples/create-get-profile.php
@@ -1,16 +1,22 @@
 <?php
+include('./test-helpers.php');
+
 # environment settings
 $pathDB = "/home/jag/.openchange/profiles.ldb";
 $profileName = 'new15';
-$username='u10';
+$username='u13';
 $password = 'a';
 $domain = 'example42';
 $realm = 'example42.com';
 $server =  '192.168.56.42';
 
 $mapiDB = new MAPIProfileDB($pathDB);
-$profile = $mapiDB->createAndGetProfile($profileName, $username, $password, $domain, $realm, $server);
-var_dump($profile);
+ok($mapiDB, "MAPIProfileDB opened for $pathDB");
 
+$profile = $mapiDB->createAndGetProfile($profileName, $username, $password, $domain, $realm, $server);
+ok($profile, "createAndGet profile for $profileName");
+
+$session = $profile->logon();
+ok($session, "Check logon with profile $profileName");
 
 ?>

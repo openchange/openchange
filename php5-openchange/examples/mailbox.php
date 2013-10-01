80d4aa5a14efe106bb68c22813079a1f1a0858f9
Javier Amor García <jamor@zentyal.com>
2013-10-01 19:11:06 +0200

    workaround against dstruction issues

--- php5-openchange/examples/mailbox.php
+++ php5-openchange/examples/mailbox.php
@@ -29,27 +29,36 @@ $inbox = $mailbox->inbox();
 ok($inbox, "Get inbox folder");
 is($inbox->getFolderType(), "IPF.Note", "Get inbox folder type");
 is($inbox->getName(), $inboxName, "Get inbox folder name");
+unset($inbox);
 
 $calendar = $mailbox->calendar();
 ok($calendar, "Get calendar folder");
 is($calendar->getFolderType(), "IPF.Appointment", "Get calendar folder type");
 is($calendar->getName(), $calendarName, "Get calendar folder name");
+unset($calendar);
 
 $tasks = $mailbox->tasks();
 ok($tasks, "Get tasks folder");
 is($tasks->getFolderType(), "IPF.Task", "Get tasks folder type");
 is($tasks->getName(), $tasksName, "Get tasks folder name");
+unset($tasks);
 
 $contacts = $mailbox->contacts();
 ok($contacts, "Get contacts folder");
 is($contacts->getFolderType(), "IPF.Contact", "Get contacts folder type");
 is($contacts->getName(), $contactsName, "Get contacts folder name");
-
+unset($contactt);
 
 $drafts = $mailbox->openFolder($draftFolderId, "IPF.Note");
 ok($drafts, "Open by ID draft folder");
+unset($drafts);
 
 ok(is_null($mailbox->openFolder($inexistentFolderId, "IPF.Note")), "Open folder with inexistent ID returns NULL");
+
+unset($mailbox);
+unset($session);
+unset($profile);
+unset($mapi);
 
 endTestSuite("mailbox.php");
 ?>

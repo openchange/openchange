98cdf470b83009f9316e9efcdcad98f74ff1650f
Jesús García Sáez <jgarcia@zentyal.com>
2013-10-29 16:10:13 +0100

    new test directory with unittest of openchange code
      * Now it only has unittest for namedprops_mysql.c from libmapistore
      * It uses check framework and cmake for building.
      * It requires to have openchange already installed locally

---
+++ test/test_common.h
@@ -0,0 +1,17 @@
+#ifndef TEST_COMMON_H_
+#define TEST_COMMON_H_
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#define MYSQL_HOST "localhost"
+#define MYSQL_USER "root"
+#define MYSQL_PASS ""
+#define MYSQL_DB   "openchange_test"
+
+#define CHECK_MYSQL_ERROR \
+	if (mysql_errno(conn)) \
+		fprintf(stderr, "Error on MySQL: %s", mysql_error(conn))
+
+#endif /* TEST_COMMON_H_ */

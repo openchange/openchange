4e4d03a94c99b6e49b9449118e6746a0371d1b69
Jesús García Sáez <jgarcia@zentyal.com>
2013-10-30 09:30:15 +0100

    add coverage flags and xml test output

--- test/openchange_test_suite.c
+++ test/openchange_test_suite.c
@@ -9,6 +9,7 @@ int main(void)
 	Suite *s = namedprops_mysql_suite();
 	SRunner *sr = srunner_create(s);
 
+	srunner_set_xml(sr, "test_results.xml");
 	srunner_run_all(sr, CK_NORMAL);
 	number_failed = srunner_ntests_failed(sr);
 	srunner_free(sr);

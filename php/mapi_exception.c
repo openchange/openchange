127f4ab37a6102ed03213e637f42db2221401eeb
Javier Amor García <jamor@zentyal.com>
2014-01-15 12:58:16 +0100

    avoid apache segfault when registering exception class

--- php/mapi_exception.c
+++ php/mapi_exception.c
@@ -3,10 +3,9 @@
 static zend_class_entry *mapi_exception_ce;
 
 void MAPIExceptionRegisterClass(TSRMLS_D) {
-	zend_class_entry e;
-
-	INIT_CLASS_ENTRY(e, "MAPIException", NULL);
-	mapi_exception_ce = zend_register_internal_class_ex(&e, (zend_class_entry*)zend_exception_get_default(TSRMLS_C), NULL TSRMLS_CC);
+	zend_class_entry ex_entry;
+	INIT_CLASS_ENTRY(ex_entry, "MAPIException", NULL);
+	mapi_exception_ce = zend_register_internal_class_ex(&ex_entry, NULL, "exception" TSRMLS_CC);
 }
 
 zend_class_entry *mapi_exception_get_default(){

0ccb8a1183db7af815c8e4a10c85c1a170e4cbaa
Jesús García Sáez <jgarcia@zentyal.com>
2014-03-17 10:14:27 +0100

    add --can-deprovision option to openchange_provision

--- python/openchange/provision.py
+++ python/openchange/provision.py
@@ -662,6 +662,19 @@ def register(setup_path, names, lp, creds, reporter=None):
                " objects (%d): %s" % ldb_error.args)
 
 
+def check_unregister(names, lp, creds):
+    """Check if we can unregister an OpenChange server.
+
+    :param names: Provision Names object
+    :param lp: Loadparm context
+    :param creds: Credentials context
+    """
+    server_uses = checkusage(names, lp, creds)
+    if (len(server_uses) > 0):
+        return ', '.join(server_uses)
+    return None
+
+
 def unregister(setup_path, names, lp, creds, reporter=None):
     """Unregisters an OpenChange server.
 
@@ -683,9 +696,9 @@ def unregister(setup_path, names, lp, creds, reporter=None):
     if reporter is None:
         reporter = TextProgressReporter()
 
-    server_uses = checkusage(names, lp, creds)
-    if (len(server_uses) > 0):
-        raise ServerInUseError(', '.join(server_uses))
+    server_uses = check_unregister(names, lp, creds)
+    if (server_uses is not None):
+        raise ServerInUseError(server_uses)
 
     try:
         # Unregister the server

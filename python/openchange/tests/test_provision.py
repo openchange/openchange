896e7333fd3dffa6a73d698ffe31f7e30bcd1f5d
Jelmer Vernooij <jelmer@samba.org>
2014-03-30 20:47:15 +0200

    openchange.tests: Fix formatting, file header.

--- python/openchange/tests/test_provision.py
+++ python/openchange/tests/test_provision.py
@@ -7,12 +7,12 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
-#   
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-#   
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
@@ -21,8 +21,12 @@ from samba import param
 from samba.credentials import Credentials
 from samba.tests import TestCaseInTempDir
 from samba.tests.samdb import SamDBTestCase
-from openchange.provision import (install_schemas, openchangedb_provision,
-    guess_names_from_smbconf, find_setup_dir)
+from openchange.provision import (
+    find_setup_dir,
+    guess_names_from_smbconf,
+    install_schemas,
+    openchangedb_provision,
+    )
 
 import os
 import shutil

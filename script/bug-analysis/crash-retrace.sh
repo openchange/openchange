dcea601866b1e7b4544bff5823f76706740b97e3
Enrique J. Hernández Blasco <ejhernandez@zentyal.com>
2014-06-03 18:01:10 +0200

    Added shell script helper to avoid required knowledge

---
+++ script/bug-analysis/crash-retrace.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+OPTIND=1
+while getopts "v" OPTION ; do # set $o to the next passed option
+  case $OPTION in
+    v)
+       verbose=-v
+       ;;
+
+    ?) echo "Invalid option $OPTION"
+       exit 1;
+       ;;
+
+    :) echo "Option $OPTION requires an argument"
+       exit 1;
+       ;;
+  esac
+done
+
+shift $(($OPTIND - 1))
+
+crashes=$1
+if [ -z "$crashes" ]; then
+    echo "Usage: $0 [-v] crash_dir_or_file"
+    exit 1
+fi
+
+APPORT_CRASHDB_CONF=crashdb.conf ./oc-crash-digger -c apport-config -C apport-sandbox-cache -S $crashes --oc-cd-conf=crash-digger.conf $verbose

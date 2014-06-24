d7a8a42fbd830dc485fc2a3feff9557308a18af9
Javier Amor García <jamor@zentyal.com>
2014-06-24 16:32:55 +0200

    oab wip

---
+++ python/oab/oab-test.py
@@ -0,0 +1,144 @@
+import unittest
+import struct
+from pprint import pprint
+
+import sys
+sys.path.append('.')
+
+class TestOAB(unittest.TestCase):
+    def _check_rdn_consistence(self, rdnContents):
+        rdnContentsSize = len(rdnContents)
+
+        nAccounts = struct.unpack('<I', rdnContents[8:12])
+        self.assertTrue(nAccounts > 0)
+
+        print 'rdnContents:'
+        pprint (rdnContents)
+
+ #       print 'rdnContents[12:16] -> '
+ #       pprint(rdnContents[12:16])
+
+        # DDD
+        oRootBytes = str(bytearray(rdnContents[12:16]))
+        print 'oRootBytes=' + str(type(oRootBytes)) + ' -> '
+        pprint(oRootBytes)
+        # DDD
+
+        oRoot = self._unpack_uint(rdnContents[12:16])
+        print 'oRoot= ' + str(type(oRoot)) + ' -> ' + str(oRoot) # DDD
+        self.assertTrue(oRoot > 16) # always will be more that 18(size header)
+
+
+
+        pdnPart = rdnContents[16:oRoot]
+        self.assertTrue(len(pdnPart) > 0)
+
+        pdnByOffset = {}
+        begSearch = 0
+        pdnOffset = 16
+        while begSearch < len(pdnPart):
+             found = pdnPart.find(b'\x00', begSearch)
+             if found == -1:
+                 self.assertTrue(False, msg='Not NUL byte find in pdn search beginning at ' + begSearch)
+                 break
+             pdnBytes = pdnPart[begSearch:found] # we not use found +1 bz we are not interested in NUL byte
+             pdnLen   =  len(pdnBytes) + 1 # +1 -> NUL
+             pdn = str(pdnBytes)
+             pdnByOffset[pdnOffset] = pdn
+
+             pdnOffset += pdnLen
+             begSearch = found + 1
+
+        pprint(pdnByOffset)
+
+        rdnPart = pdnPart[oRoot:] # DDd
+        pprint (rdnPart) # DDD
+
+        # checking using prev/next links
+        # XXX degenerate tree; tree not checked
+        prevLink = 0
+        nextLink = oRoot
+
+
+        while nextLink != 0:
+            print "RDN with offset " + str(nextLink)
+
+            oPrev = self._unpack_uint(rdnContents[nextLink+12:nextLink+16])
+            self.assertTrue(oPrev < rdnContentsSize)
+            self.assertTrue(oPrev == prevLink)
+
+            oNext = self._unpack_uint(rdnContents[nextLink+16:nextLink+20])
+            self.assertTrue(oNext < rdnContentsSize)
+
+            oParentDN = self._unpack_uint(rdnContents[nextLink+20:nextLink+24])
+            print 'oParentDN ' + str(oParentDN)
+            self.assertTrue(oParentDN in pdnByOffset)
+
+            found = rdnContents.find(b'0', nextLink+24)
+            rdnBytes  =  rdnContents[nextLink+24:found]
+            rdn = str(rdnBytes)
+            print 'RDN=' + rdn
+
+            prevLink = nextLink
+            nextLink = oNext
+
+
+
+    def test_rdn_consistence(self):
+        cases = [
+            (
+
+
+            )
+
+
+#          bytearray(b'\x0e\x00\x00\x00\x00\x00\x00\x00\n\x00\x00\x00l\x00\x00\x00OU=\xd1\x80\xd1\x83\xd1\x81\xd0\xba\xd0\xb8,DC=zentyal-domain,DC=lan\x00zentyal-domain.lan\x00CN=Users,DC=zentyal-domain,DC=lan\x00\x00\x00\x00\x00\x90\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x90\x00\x00\x00\x10\x00\x00\x00asdvsdffv a0l\x00\x00\x00\xac\x00\x00\x00\x00\x00\x00\x00l\x00\x00\x00\xac\x00\x00\x007\x00\x00\x00u2@0\x90\x00\x00\x00\xd2\x00\x00\x00\x00\x00\x00\x00\x90\x00\x00\x00\xd2\x00\x00\x00J\x00\x00\x00Administrator0\xac\x00\x00\x00\xf9\x00\x00\x00\x00\x00\x00\x00\xac\x00\x00\x00\xf9\x00\x00\x007\x00\x00\x00administrator@0\xd2\x00\x00\x00\x1c\x01\x00\x00\x00\x00\x00\x00\xd2\x00\x00\x00\x1c\x01\x00\x00J\x00\x00\x00sdsdsd dfg0\xf9\x00\x00\x008\x01\x00\x00\x00\x00\x00\x00\xf9\x00\x00\x008\x01\x00\x007\x00\x00\x00u1@0\x1c\x01\x00\x00V\x01\x00\x00\x00\x00\x00\x00\x1c\x01\x00\x00V\x01\x00\x00J\x00\x00\x00Guest08\x01\x00\x00u\x01\x00\x00\x00\x00\x00\x008\x01\x00\x00u\x01\x00\x007\x00\x00\x00guest@0V\x01\x00\x00\x92\x01\x00\x00\x00\x00\x00\x00V\x01\x00\x00\x92\x01\x00\x00J\x00\x00\x00a ad0u\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00u\x01\x00\x00\x00\x00\x00\x007\x00\x00\x00ad@0')
+          ]
+        for case in cases:
+            self._check_rdn_consistence(case)
+
+    def _check_anr_consistence(self, anr_contents):
+        anr_contents_size = len(anr_contents)
+        nAccounts = self._unpack_uint(anr_contents[8:12])
+        self.assertTrue(nAccounts > 0)
+
+
+        # checking using prev/next links
+        # XXX degenerate tree; tree not checked
+        prevLink = 0
+        nextLink = 16 # 16b headert
+        while nextLink != 0:
+            print "ANR with offset " + str(nextLink)
+
+            oPrev = self._unpack_uint(anr_contents[nextLink+12:nextLink+16])
+            self.assertTrue(oPrev < anr_contents_size)
+            self.assertTrue(oPrev == prevLink)
+
+            oNext = self._unpack_uint(anr_contents[nextLink+16:nextLink+20])
+            self.assertTrue(oNext < anr_contents_size)
+
+            found = anr_contents.find(b'0', nextLink+24)
+            anr_bytes  =  anr_contents[nextLink+24:found]
+            anr = str(anr_bytes)
+            print 'ANR=' + anr
+
+            prevLink = nextLink
+            nextLink = oNext
+
+    def test_anr_consistence(self):
+        cases = [
+            bytearray(b"\x0e\x00\x00\x00\x00\x00\x00\x00\x04\x00\x00\x00\x00\x00\x00\x00#\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00#\x00\x00\x00u20\x0c\x00\x00\x00\'\x00\x00\x00\x00\x00\x00\x00\x0c\x00\x00\x00\'\x00\x00\x00ibubui0\x0c\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x0c\x00\x00\x00\x00\x00\x00\x00u10\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00a0")
+
+            ]
+        for case in cases:
+            self._check_anr_consistence(case)
+
+
+
+    def _unpack_uint(self, barray):
+        bstr = str(bytearray(barray[0:4]))
+        return struct.unpack('<I', bstr)[0]
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ python/oab/oab.py
@@ -0,0 +1,341 @@
+import struct
+
+# TESTY
+from samba.param import LoadParm
+from samba.auth import system_session
+from samba import samdb, ldb
+from pprint import pprint
+
+
+class OAB:
+    MAX_UL_TOT_RECORDS = 16777212
+
+    def __init__(self):
+        pass
+
+    def createFiles(self, accounts, directory):
+        nAccounts = len(accounts)
+        if (nAccounts == 0):
+            raise Exception('Trying to create OAB files without accounts');
+        elif (nAccounts  > OAB.MAX_UL_TOT_RECORDS):
+            # XXX remember to do the check fopr attr in individual files
+            # XXX removing excessive accounts
+            nAccounts = nAccounts[0:OAB.MAX_UL_TOT_RECORDS-1]
+
+        browseFileIndex, browseFileContents = self._browseFileContents(accounts)
+        print 'Browse file:'
+        pprint(browseFileContents)
+
+        rdnFileContents    = self._rdnFileContents(accounts, browseFileIndex, browseFileContents)
+        print 'RDN file:'
+        pprint(rdnFileContents)
+
+        anrFileContents = self._anrFileContents(accounts, browseFileIndex, browseFileContents)
+        print 'ANR file:'
+        pprint(anrFileContents)
+
+    def _browseFileContents(self, accounts):
+        contents = self._browseFileHeader(accounts)
+        index    = {}
+        for acc in accounts:
+            record  = self._browseRecord(acc)
+            index[record['samAccountName']] = len(contents)
+            contents += record
+
+        # calculate RDN hash for put in browse file
+        return (contents, index)
+
+    def _browseFileHeader(self, accounts):
+        header = bytearray(10)
+        # ulVersion
+        header[0] =  0xA
+        header[1] =  0x0
+        # ulSerial
+        # TODO 2.3.3
+        packedNAccounts = struct.pack('<I', len(accounts))
+        header[4] = packedNAccounts[0]
+        header[5] = packedNAccounts[1]
+        header[6] = packedNAccounts[2]
+        header[7] = packedNAccounts[3]
+        return header
+
+    def _browseRecord(self, account):
+        record = bytearray(32)
+        # TODO oRDN 4
+        # TODO oDetails 4
+        # TODO cbDetails 2
+        # bDispType
+        if account['type'] == 'mailuser':
+            record[10] = 0x0
+        elif account['type'] == 'distlist':
+            record[10] = 0x1
+        else:
+            raise Exception("Unknow account type " + record['type'])
+
+        # a 1b 1 (can receive rich content)
+        if account['SendRichInfo']:
+            record[11] = 0x80
+        else:
+            record[11] = 0x00
+        # mailobj (7b)
+        if account['type'] == 'mailuser':
+            record[11] += 0x06
+        elif account['type'] == 'distlist':
+            record[11] += 0x08
+        else:
+            raise Exception("Unknow account type " + record['type'])
+        # oSmtp (4 bytes)
+        # oAlias (4 bytes): A 32-bit unsigned integer that specifies the offset of the alias record in the
+        # ANR Index file.
+
+        # oLocation (4 bytes): A 32-bit unsigned integer that specifies the offset of the office location
+        # record in the ANR Index file.
+
+        # oSurname (4 bytes): A 32-bit unsigned integer that specifies the offset of the surname record
+        # in the ANR Index file.
+        return record
+
+    def _rdnFileContents(self, accounts, browseFileIndex, browseFileContents):
+        # TODO : put also rdn for @ mail addresses
+        contents = self._rdnHeader(accounts)
+
+        pdn = self._rdnPdnRecords(accounts, len(contents))
+        pprint(pdn)
+        contents += pdn[1]
+        offsetByPdn = pdn[0]
+
+        # now we have the offset of the first RDN and we can set oRoot
+        print "oRoot len(ciontnts) _> " + str(len(contents))
+        oRootPacked = self._pack_uint(len(contents))
+        contents[12:16]  = oRootPacked[0:4]
+
+        oPrev = 0;
+        oNextBase = len(contents)
+        lastAccount = len(accounts) - 1
+        for i in range(0, lastAccount +1):
+            acc = accounts[i]
+            iBrowse = browseFileIndex[acc['samAccountName']]
+
+            rdn, pdn = acc['dn'].split(',', 1)
+            rdn = rdn.split('=', 1)[1]
+
+            record = self._rdnRecord(rdn, pdn, offsetByPdn, oPrev, oNextBase, iBrowse)
+            oPrev = len(contents)
+            contents += record
+            if i == lastAccount:
+                oNextBase = 0
+            else:
+                oNextBase = len(contents)
+
+            rdn, pdn = acc['mail'].split('@', 1)
+            rdn += '@'
+            record = self._rdnRecord(rdn, pdn, offsetByPdn, oPrev, oNextBase, iBrowse)
+            oPrev = len(contents)
+            contents += record
+            record_offset = len(contents)
+            oNextBase = record_offset # no effect if last account
+            # add oSMTP entry to browse file
+            browseFileContents[iBrowse+12:iBrowse+16] = self._pack_uint(record_offset)
+
+        return contents
+
+
+    def _rdnHeader(self, accounts):
+        header = bytearray(16)
+        # ulVersion
+        header[0:4] = 0x0E, 0x00, 0x00, 0x00
+
+        # ulSerial (4 bytes): A 32-bit hexadecimal string that specifies the hash of the RDN (1) values for
+        #   the current set of OAB records. The value of this field is calculated as specified in section
+        # TODO
+
+        # ulTotRecs (4 bytes)
+        nEntries = len(accounts)*2 # each account has a dn and mail attrs
+        header[8:12] = self._pack_uint(nEntries)
+
+        # oRoot (4 bytes): A 32-bit unsigned integer that specifies the offset of the root RDN2_REC
+        # to be calculated and set later
+        return header
+
+        # returns the tuple (offsetByPdn, pdnRecordsByteArray)
+    def _rdnPdnRecords(self, accounts, offset):
+        offsetByPdn = {}
+        records = bytearray();
+        for acc in accounts:
+            newPdns = []
+            newPdns.append(acc['dn'].split(',', 1)[1])
+            newPdns.append(acc['mail'].split('@', 1)[1])
+
+            for pdn in newPdns:
+                if pdn in offsetByPdn:
+                    break
+                pdnBytes = bytearray(pdn)
+                pdnBytes.append(0x00);
+                offsetByPdn[pdn] = offset;
+                offset += len(pdnBytes)
+                records += pdnBytes
+
+        return (offsetByPdn, records)
+
+    def _rdnRecord(self, rdn, pdn, offsetByPdn, oPrev, oNextBase, iBrowse):
+        record = bytearray(24) # min size, RDN records are variable
+        # XXX degenerate tree: oLT, rLT -> oPrev, oNext
+        # oLT 4b
+        record[0:4] = struct.pack('<I', oPrev)[0:4]
+        # rLT 4b
+        # iBrowse (4 bytes):
+        record[8:12] = self._pack_uint(iBrowse)
+        # oPrev (4 bytes)
+        record[12:16] = struct.pack('<I', oPrev)[0:4]
+        # oNext (4 bytes)
+        # set later
+
+        # oParentDN (4 bytes)
+        print rdn + ' pdn: ' + pdn + ' offset ' + str(offsetByPdn[pdn])
+        record[20:24] = self._pack_uint(offsetByPdn[pdn])
+
+        # acKey (variable):
+        record +=  bytearray(rdn) + b'0'
+
+        oNext = 0
+        if (oNextBase != 0):
+            oNext = oNextBase + len(record)
+        # rLT
+        record[4:8] = struct.pack('<I', oNext)[0:4]
+        # oNext
+        record[16:20] = struct.pack('<I', oNext)[0:4]
+
+        return record
+
+    def _anrFileContents(self, accounts):
+        contents = self._anrHeader(accounts)
+
+        oPrev = 0;
+        oNextBase = 0
+        lastAccount = len(accounts) - 1
+        for i in range(0, lastAccount +1):
+            acc = accounts[i]
+            if i == lastAccount:
+                oNextBase = 0
+            else:
+                oNextBase = len(contents)
+            for attr in ['displayName', 'sn', 'office', 'alias']:
+                if not(attr in acc):
+                    continue
+
+                attrValue = acc[attr]
+                record    = self._anrRecord(attrValue, oPrev, oNextBase, attr == 'alias')
+                contents += record
+                oPrev = oNextBase
+
+        return contents
+
+    def _anrHeader(self, accounts):
+        header = bytearray(12)
+        # ulVersion (4 bytes):
+        header[0:4] = self._pack_uint(0x0000000E)
+        # ulSerial (4 bytes)
+        # to be calculated later
+        # ulTotRecs (4 bytes):
+        # XXX attr for alias
+        nRecords = self._count_attributes(accounts, ['displayName', 'sn', 'office', 'alias'])
+        header[8:12] = self._pack_uint(nRecords)
+
+        return header
+
+    def _anrRecord(self, attrValue, oPrev, oNextBase, alias):
+        record = bytearray(20) # 20 is only the fixed  bytes
+        # XXX degenerate tree: oLT, oGT -> oPrev, oNext
+        record[0:4] = self._pack_uint(oPrev) #oLt
+        # oGT (4 bytes) , later
+        # iBrowse 3b XX TODO
+        # a/b 1b
+        ab = 0x00
+        if alias:
+            ab = 0x80
+        record[11] = ab
+        record[12:16] = self._pack_uint(oPrev) # oPrev
+        # oNext (4 bytes), later
+        # acKey (variable)
+        record +=  bytearray(attrValue) + b'0'
+
+        oNext = 0
+        if (oNextBase != 0):
+            oNext = oNextBase + len(record)
+        # rLT
+        record[4:8] = self._pack_uint(oNext)
+        # oNext
+        record[16:20] = self._pack_uint(oNext)
+
+        return record
+
+    def _pack_uint(self, uint):
+        return struct.pack('<I', uint)
+
+    def _count_attributes(self, accounts, attrs):
+        count = 0
+        for acc in accounts:
+            for attr in attrs:
+                if attr in acc:
+                    count += 1
+        return count
+
+    def endClass():
+        pass
+
+
+
+
+def accountsList():
+    samdb_url = '/var/lib/samba/private/sam.ldb'
+    db = samdb.SamDB(url=samdb_url, session_info=system_session(), lp=LoadParm())
+
+    accounts = []
+    basedn =  db.domain_dn()
+    res = db.search(base=basedn, scope=ldb.SCOPE_SUBTREE, expression="(|(objectclass=user))(objectclass=group)))")
+
+    # XXX add subnames, alias, office
+
+    for entry in res:
+        mailAttr =  entry.get('mail')
+        if not mailAttr:
+            continue
+
+        account = {}
+        account['mail'] = mailAttr.get(0)
+        account['dn'] = str(entry.dn)
+        account_type = ''
+        for oclass in entry.get('objectclass'):
+            if oclass == "user":
+                account_type = 'mailuser'
+                break;
+            elif oclass == "group":
+                account_type = 'distlist'
+                break
+
+            if account_type == '':
+                # Not valid objectclass!
+                continue
+
+        account['type'] = account_type
+        account['samAccountName']= entry.get('samAccountName').get(0)
+        account['displayName'] = account['samAccountName']
+        account['SendRichInfo'] = 1 # for now always on
+        sn =  entry.get('sn')
+        if sn:
+            account['sn'] = sn.get(0)
+        office = entry.get('physicalDeliveryOfficeName')
+        if office:
+            account['office'] = office.get(0)
+        # XXX TODO alias mail otherMailbox?
+        # account['alias']
+        accounts.append(account)
+
+    return accounts
+
+
+# test code
+accounts = accountsList()
+pprint(accounts)
+oab = OAB()
+oab.createFiles(accounts, '/tmp')

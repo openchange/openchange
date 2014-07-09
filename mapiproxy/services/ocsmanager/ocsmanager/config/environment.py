7b4ce79d1da8f36e85c1dd99a219b9b465f2d923
Enrique J. Hernández Blasco <ejhernandez@zentyal.com>
2014-07-09 08:35:04 +0200

    Added copyright note

--- mapiproxy/services/ocsmanager/ocsmanager/config/environment.py
+++ mapiproxy/services/ocsmanager/ocsmanager/config/environment.py
@@ -1,3 +1,24 @@
+# environment.py
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2011-2014  Julien Kerihuel <jkerihuel@openchange.org>
+#                          Jean Raby <jraby@inverse.ca>
+#                          Jesús García Saez <jgarcia@zentyal.com>
+#                          Enrique J. Hernández <ejhernandez@zentyal.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 """Pylons environment configuration"""
 import os
 

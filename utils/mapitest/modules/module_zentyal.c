f5ca7d1ed704d377d50643be61ac771f3d64afcf
Julien Kerihuel <j.kerihuel@openchange.org>
2014-07-30 13:03:17 +0200

    Fix expected returned value for mapitest zentyal 1872

--- utils/mapitest/modules/module_zentyal.c
+++ utils/mapitest/modules/module_zentyal.c
@@ -62,7 +62,7 @@ _PUBLIC_ bool mapitest_zentyal_1872(struct mapitest *mt)
 	retval = nspi_QueryRows(nspi_ctx, mem_ctx, NULL, MIds, 1, &RowSet);
 	MAPIFreeBuffer(RowSet);
 	mapitest_print_retval_clean(mt, "1872", retval);
-	if (retval != MAPI_E_SUCCESS) {
+	if (retval != MAPI_E_INVALID_PARAMETER) {
 		MAPIFreeBuffer(MIds);
 		talloc_free(mem_ctx);
 		return false;

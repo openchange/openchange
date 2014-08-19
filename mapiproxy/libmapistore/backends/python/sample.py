83ae513f3a6b63d282ce11ee76bf207335d5dedf
Julien Kerihuel <j.kerihuel@openchange.org>
2014-08-19 14:25:06 +0200

    - Add Copyright header - Import mapistore python bindings to access further symbols/enum

--- mapiproxy/libmapistore/backends/python/sample.py
+++ mapiproxy/libmapistore/backends/python/sample.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python
+# OpenChange python sample backend
+#
+# Copyright (C) Julien Kerihuel <j.kerihuel@openchange.org> 2014
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 import os,sys
 
 root = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(os.sep.join((root, '..', '..')))
+
+from openchange import mapistore
 
 class BackendObject(object):
 

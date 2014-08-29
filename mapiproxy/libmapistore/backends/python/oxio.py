5509b729de149c28981c777d0629613d7a873a87
Kamen Mazdrashki <kmazdrashki@zentyal.com>
2014-08-29 15:19:04 +0200

    mapistore/oxio: move logger initialization in separate function
    It is still a global logger - not very nice but will do the job

--- mapiproxy/libmapistore/backends/python/oxio.py
+++ mapiproxy/libmapistore/backends/python/oxio.py
@@ -34,22 +34,29 @@ from openchange import mapistore
 
 import logging
 
-# create logger with 'spam_application'
-logger = logging.getLogger('OXIO')
-logger.setLevel(logging.DEBUG)
-# create file handler which logs even debug messages
-fh = logging.FileHandler('/tmp/oxio.log')
-fh.setLevel(logging.DEBUG)
-# create console handler with a higher log level
-ch = logging.StreamHandler()
-ch.setLevel(logging.ERROR)
-# create formatter and add it to the handlers
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fh.setFormatter(formatter)
-ch.setFormatter(formatter)
-# add the handlers to the logger
-logger.addHandler(fh)
-logger.addHandler(ch)
+
+def _initialize_logger():
+    # create logger with 'spam_application'
+    logger = logging.getLogger('OXIO')
+    logger.setLevel(logging.DEBUG)
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler('/tmp/oxio.log')
+    fh.setLevel(logging.DEBUG)
+    # create console handler with a higher log level
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    # add the handlers to the logger
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+    return logger
+
+
+# create global logger isntance
+logger = _initialize_logger()
 
 
 class _OxioConn(object):

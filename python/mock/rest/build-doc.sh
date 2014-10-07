1249050470cb4c53bac392f43dbe4aebf9a75c45
Kamen Mazdrashki <kmazdrashki@zentyal.com>
2014-10-07 05:46:55 +0200

    mock/rest: build-doc string automatically updates documentation from GitHub

--- python/mock/rest/build-doc.sh
+++ python/mock/rest/build-doc.sh
@@ -4,6 +4,11 @@ out_dir=./out~
 www_dir=../www
 
 pushd doc-src
+echo "Update documentation source"
+wget https://raw.githubusercontent.com/openchange/website/jkerihuel/mapistore-python/api/mapistore_http/Makefile -O Makefile
+wget https://raw.githubusercontent.com/openchange/website/jkerihuel/mapistore-python/api/mapistore_http/source/index.rst -O source/index.rst
+wget https://raw.githubusercontent.com/openchange/website/jkerihuel/mapistore-python/api/mapistore_http/source/conf.py -O source/conf.py
+
 make singlehtml BUILDDIR=./out~
 cp $out_dir/singlehtml/index.html $www_dir/
 cp -r $out_dir/singlehtml/_static $www_dir/

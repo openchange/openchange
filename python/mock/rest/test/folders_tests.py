cf83f0e9b24abdfda6928544ca01eeafb67516ab
Kamen Mazdrashki <kmazdrashki@zentyal.com>
2014-10-09 06:07:37 +0200

    mock/rest:  Folders module Interface test case implementation

---
+++ python/mock/rest/test/folders_tests.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2014  Kamen Mazdrashki <kamenim@openchange.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""
+Unit testing for Openchange REST API server
+This module tests "/folders/" module.
+For reference see <>
+"""
+
+import unittest
+import basetest
+
+
+class FoldersInterfaceTestCase(basetest.MockApiBaseTestCase):
+
+    def test_create(self):
+        """Folder creation test"""
+        status, text, headers = self._create_test_folder(1, 'folder-create', 'create comment')
+        self.assertEquals(status, 200)
+        self.assertIn('id', self._to_json_ret(text))
+
+    def test_get(self):
+        # create some test item to play with
+        status, text, headers = self._create_test_folder(name='folder-get', comment='folder-get comment')
+        item = self._to_json_ret(text)
+        # fetch the Folder
+        path = '/folders/%d/?properties=id,type,PidTagDisplayName,PidTagComment' % item['id']
+        status, text, headers = self.get_req(path)
+        self.assertEqual(status, 200)
+        res = self._to_json_ret(text)
+        self.assertEqual(res['id'], item['id'])
+        self.assertEqual(res['type'], 'folder')
+        self.assertEqual(res['PidTagDisplayName'], 'folder-get')
+        self.assertEqual(res['PidTagComment'], 'folder-get comment')
+
+    def test_update(self):
+        # create some test item to play with
+        status, text, headers = self._create_test_folder(name='folder-update', comment='folder-update comment')
+        item = self._to_json_ret(text)
+        # update the Folder
+        data = {
+            'PidTagComment': 'Updated body'
+        }
+        path = '/folders/%d/' % item['id']
+        status, text, headers = self.put_req(path, data)
+        self.assertEqual(status, 201)
+        self.assertEqual(text, "")
+
+    def test_existing_folder(self):
+        status, text, headers = self.head_req('/folders/1/')
+        self.assertEquals(status, 200, "Unexpected status code")
+        self.assertEquals(text, '', "Empty response expected")
+
+    def test_delete(self):
+        # create some test item to play with
+        status, text, headers = self._create_test_folder(name='folder-delete', comment='delete comment')
+        item = self._to_json_ret(text)
+        # check message exists
+        path = '/folders/%d/' % item['id']
+        status, text, headers = self.delete_req(path)
+        self.assertEqual(status, 204)
+        self.assertEqual(text, "")
+
+    def _create_test_folder(self, parent_id=1, name='test folder', comment='folder comment'):
+        data = {
+            'parent_id': parent_id,
+            'PidTagDisplayName': name,
+            'PidTagComment': comment
+        }
+        return self.post_req('/folders/', data)
+
+
+if __name__ == '__main__':
+    unittest.main()

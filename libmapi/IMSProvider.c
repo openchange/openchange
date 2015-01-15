455efb0771bd5c5d988a1d96efea439090a3b081
Julien Kerihuel <j.kerihuel@openchange.org>
2015-01-15 11:33:25 +0100

    Fix termination of binding string with RoH options.

--- libmapi/IMSProvider.c
+++ libmapi/IMSProvider.c
@@ -91,6 +91,7 @@ static char *build_binding_string(struct mapi_context *mapi_ctx,
 				  struct mapi_profile *profile)
 {
 	char	*binding;
+	int	binding_len = 0;
 
 	/* Sanity Checks */
 	if (!profile) return NULL;
@@ -130,7 +131,12 @@ static char *build_binding_string(struct mapi_context *mapi_ctx,
 		if (!binding) return NULL;
 	}
 
-	binding = talloc_strdup_append(binding, "]");
+	binding_len = strlen(binding);
+	if (binding[binding_len-1] == ',') {
+		binding[binding_len-1] = ']';
+	} else {
+		binding = talloc_strdup_append(binding, "]");
+	}
 	if (!binding) return NULL;
 
 	return binding;

7179a3600dda35a60561f842684b201264917d35
Jesús García Sáez <jgarcia@zentyal.com>
2015-02-10 19:47:27 +0100

    openchangedb: create_folder must use the given change number
    Now a new change number was being generated and the given one
    was ignored.

--- mapiproxy/libmapiproxy/backends/openchangedb_mysql.c
+++ mapiproxy/libmapiproxy/backends/openchangedb_mysql.c
@@ -1784,7 +1784,7 @@ static enum MAPISTATUS create_mailbox(struct openchangedb_context *self,
 static enum MAPISTATUS create_folder(struct openchangedb_context *self,
 				     const char *username,
 				     uint64_t pfid, uint64_t fid,
-				     uint64_t changeNumber,
+				     uint64_t change_number,
 				     const char *MAPIStoreURI, int systemIdx)
 {
 	TALLOC_CTX	*mem_ctx;
@@ -1792,7 +1792,6 @@ static enum MAPISTATUS create_folder(struct openchangedb_context *self,
 	enum MAPISTATUS	retval;
 	char		*sql, *values_sql, *value;
 	const char	**l;
-	uint64_t	change_number = 0;
 	time_t		unix_time;
 	NTTIME		now;
 
@@ -1807,9 +1806,6 @@ static enum MAPISTATUS create_folder(struct openchangedb_context *self,
 		OPENCHANGE_RETVAL_ERR(MAPI_E_CALL_FAILED, mem_ctx);
 	}
 	unix_to_nt_time(&now, unix_time);
-
-	retval = get_new_changeNumber(self, username, &change_number);
-	OPENCHANGE_RETVAL_IF(retval != MAPI_E_SUCCESS, retval, mem_ctx);
 
 	if (is_public_folder(fid)) {
 		// Insert row in folders

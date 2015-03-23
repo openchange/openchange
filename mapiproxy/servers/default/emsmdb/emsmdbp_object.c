a68088ab984cd10f3d40b4f23ce521743ca5dc43
Julien Kerihuel <j.kerihuel@openchange.org>
2015-03-23 17:15:19 +0100

    Update Copyright

--- mapiproxy/servers/default/emsmdb/emsmdbp_object.c
+++ mapiproxy/servers/default/emsmdb/emsmdbp_object.c
@@ -3,7 +3,7 @@
 
    EMSMDBP: EMSMDB Provider implementation
 
-   Copyright (C) Julien Kerihuel 2009-2014
+   Copyright (C) Julien Kerihuel 2009-2015
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by

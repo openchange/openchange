openchange/openchange

commit 63f2136a25ce1d8283f1334ad067010c7bf2644a

Reuse a folder as special folder if one already exists on Logon
According to [MS-OXOSFLD] Section 3.1.4.1, the special folder must be reused
if one already exists by that name.

With this change, if there is a folder in OpenChangeDB with this name,
it will be used. So no more `Deleted Items (1)`-like folders.

mapiproxy/servers/default/emsmdb/emsmdbp_provisioning.c
@@ -520,8 +520,6 @@ FolderId: 0x67ca828f02000001      Display Name: "                        ";  Con
 				inbox_fid = current_fid;
 			}
 		} else {
-			mapistore_indexing_get_new_folderID_as_user(emsmdbp_ctx->mstore_ctx, username, &current_fid);
-			openchangedb_get_new_changeNumber(emsmdbp_ctx->oc_ctx, username, &current_cn);
 			current_name = folder_names[i];
 
 			switch (i) {
@@ -552,15 +550,43 @@ FolderId: 0x67ca828f02000001      Display Name: "                        ";  Con
 				mapistore_url = talloc_asprintf(mem_ctx, "%s0x%"PRIx64"/", fallback_url, current_fid);
 			}
 
-			/* Ensure the name is unique */
-			base_name = current_name;
-			j = 1;
-			while (openchangedb_get_fid_by_name(emsmdbp_ctx->oc_ctx, username, ipm_fid, current_name, &found_fid) == MAPI_E_SUCCESS) {
-				current_name = talloc_asprintf(mem_ctx, "%s (%d)", base_name, j);
-				j++;
+			/* According to [MS-OXOSFLD] Section 3.1.4.1, the special folder must be reused
+			   if one already exists by that name */
+			ret = openchangedb_get_fid_by_name(emsmdbp_ctx->oc_ctx, username, ipm_fid, current_name, &found_fid);
+			if (ret == MAPI_E_NOT_FOUND) {
+				retval = mapistore_indexing_get_new_folderID_as_user(emsmdbp_ctx->mstore_ctx, username, &current_fid);
+				if (retval != MAPISTORE_SUCCESS) {
+					OC_DEBUG(1, "Error getting new folder id: %s", mapistore_errstr(retval));
+					goto error;
+				}
+				ret = openchangedb_get_new_changeNumber(emsmdbp_ctx->oc_ctx, username, &current_cn);
+				if (ret != MAPI_E_SUCCESS) {
+					OC_DEBUG(1, "Error getting new change number: %s", mapi_get_errstr(ret));
+					goto error;
+				}
+				ret = openchangedb_create_folder(emsmdbp_ctx->oc_ctx, username, ipm_fid, current_fid, current_cn, mapistore_url, i);
+				if (ret != MAPI_E_SUCCESS) {
+					OC_DEBUG(1, "Error creating OpenChangeDB folder: %s", mapi_get_errstr(ret));
+					goto error;
+				}
+			} else if (ret == MAPI_E_SUCCESS) {
+				current_fid = found_fid;
+				ret = openchangedb_set_mapistoreURI(emsmdbp_ctx->oc_ctx, username, current_fid, mapistore_url);
+				if (ret != MAPI_E_SUCCESS) {
+					OC_DEBUG(1, "Error setting new MAPIStore URI %s: %s", mapistore_url, mapi_get_errstr(ret));
+					goto error;
+				}
+				ret = openchangedb_set_system_idx(emsmdbp_ctx->oc_ctx, username, current_fid, i);
+				if (ret != MAPI_E_SUCCESS) {
+					OC_DEBUG(1, "Error setting new system index %d: %s", i, mapi_get_errstr(ret));
+					goto error;
+				}
+				/* change number is updated by set_folder_properties */
+			} else {
+				OC_DEBUG(1, "Error trying to get fid by name (%s): %s", current_name, mapi_get_errstr(ret));
+				goto error;
 			}
 
-			openchangedb_create_folder(emsmdbp_ctx->oc_ctx, username, ipm_fid, current_fid, current_cn, mapistore_url, i);
 			property_row.lpProps[0].value.lpszW = current_name;
 			openchangedb_set_folder_properties(emsmdbp_ctx->oc_ctx, username, current_fid, &property_row);
 
@@ -595,9 +621,6 @@ FolderId: 0x67ca828f02000001      Display Name: "                        ";  Con
 			property_row.cValues = 2;
 			property_row.lpProps[0].ulPropTag = PR_DISPLAY_NAME_UNICODE;
 
-			mapistore_indexing_get_new_folderID_as_user(emsmdbp_ctx->mstore_ctx, username, &current_fid);
-			openchangedb_get_new_changeNumber(emsmdbp_ctx->oc_ctx, username, &current_cn);
-
 			current_name = current_folder->name;
 			current_entry = main_entries[current_folder->role];
 			if (current_entry) {
@@ -610,17 +633,45 @@ FolderId: 0x67ca828f02000001      Display Name: "                        ";  Con
 				mapistore_url = talloc_asprintf(mem_ctx, "%s0x%"PRIx64"/", fallback_url, current_fid);
 			}
 
-			/* Ensure the name is unique */
-			base_name = current_name;
-			j = 1;
-			while (openchangedb_get_fid_by_name(emsmdbp_ctx->oc_ctx, username, ipm_fid, current_name, &found_fid) == MAPI_E_SUCCESS) {
-				current_name = talloc_asprintf(mem_ctx, "%s (%d)", base_name, j);
-				j++;
+			/* According to [MS-OXOSFLD] Section 3.1.4.1, the special folder must be reused
+			   if one already exists by that name */
+			ret = openchangedb_get_fid_by_name(emsmdbp_ctx->oc_ctx, username, ipm_fid, current_name, &found_fid);
+			if (ret == MAPI_E_NOT_FOUND) {
+				retval = mapistore_indexing_get_new_folderID_as_user(emsmdbp_ctx->mstore_ctx, username, &current_fid);
+				if (retval != MAPISTORE_SUCCESS) {
+					OC_DEBUG(1, "Error getting new folder id: %s", mapistore_errstr(retval));
+					goto error;
+				}
+				ret = openchangedb_get_new_changeNumber(emsmdbp_ctx->oc_ctx, username, &current_cn);
+				if (ret != MAPI_E_SUCCESS) {
+					OC_DEBUG(1, "Error getting new change number: %s", mapi_get_errstr(ret));
+					goto error;
+				}
+				ret = openchangedb_create_folder(emsmdbp_ctx->oc_ctx, username, ipm_fid, current_fid, current_cn, mapistore_url, i);
+				if (ret != MAPI_E_SUCCESS) {
+					OC_DEBUG(1, "Error creating OpenChangeDB folder: %s", mapi_get_errstr(ret));
+					goto error;
+				}
+			} else if (ret == MAPI_E_SUCCESS) {
+				current_fid = found_fid;
+				ret = openchangedb_set_mapistoreURI(emsmdbp_ctx->oc_ctx, username, current_fid, mapistore_url);
+				if (ret != MAPI_E_SUCCESS) {
+					OC_DEBUG(1, "Error setting new MAPIStore URI %s: %s", mapistore_url, mapi_get_errstr(ret));
+					goto error;
+				}
+				ret = openchangedb_set_system_idx(emsmdbp_ctx->oc_ctx, username, current_fid, i);
+				if (ret != MAPI_E_SUCCESS) {
+					OC_DEBUG(1, "Error setting new system index %d: %s", i, mapi_get_errstr(ret));
+					goto error;
+				}
+				/* change number is updated by set_folder_properties */
+			} else {
+				OC_DEBUG(1, "Error trying to get fid by name (%s): %s", current_name, mapi_get_errstr(ret));
+				goto error;
 			}
 
 			property_row.lpProps[0].value.lpszW = current_name;
 			property_row.lpProps[1].value.lpszW = container_classes[current_folder->role];
-			openchangedb_create_folder(emsmdbp_ctx->oc_ctx, username, ipm_fid, current_fid, current_cn, mapistore_url, i);
 			openchangedb_set_folder_properties(emsmdbp_ctx->oc_ctx, username, current_fid, &property_row);
 
 			/* instantiate the new folder in the backend to make sure it is initialized properly */
@@ -689,6 +740,7 @@ FolderId: 0x67ca828f02000001      Display Name: "                        ";  Con
 				while (openchangedb_get_fid_by_name(emsmdbp_ctx->oc_ctx, username, ipm_fid, current_name, &found_fid) == MAPI_E_SUCCESS) {
 					current_name = talloc_asprintf(mem_ctx, "%s (%d)", base_name, j);
 					j++;
+					OC_DEBUG(5, "Secondary entry collides in its name (%s) with 0x%"PRIx64, base_name, found_fid);
 				}
 				property_row.lpProps[0].value.lpszW = current_name;
 

commit 9b9fbfd1df046fc8391831c15f6b8f97f5814991

Refactor provision to use current_fid when it is set
This fixes a regression introduced by 63f2136a when
we want to reuse a folder as special folder if one
already exists on Logon.

Without this patch, the client gets messed up because
the fid set is not corresponding to the right folder,
making appear MAPI_E_NOT_FOUND opening folders and
incongruent information in oxcfxics protocol when
hierarchy was synched.

An example was the creation the Calendar, Contacts
and Tasks special folders by the client ignoring the
one we create on first Logon.

mapiproxy/servers/default/emsmdb/emsmdbp_provisioning.c
@@ -525,7 +525,6 @@ FolderId: 0x67ca828f02000001      Display Name: "                        ";  Con
 			switch (i) {
 			case EMSMDBP_INBOX:
 				current_entry = main_entries[MAPISTORE_MAIL_ROLE];
-				inbox_fid = current_fid;
 				break;
 			case EMSMDBP_OUTBOX:
 				current_entry = main_entries[MAPISTORE_OUTBOX_ROLE];
@@ -546,9 +545,6 @@ FolderId: 0x67ca828f02000001      Display Name: "                        ";  Con
 				}
 				mapistore_url = current_entry->url;
 			}
-			else {
-				mapistore_url = talloc_asprintf(mem_ctx, "%s0x%"PRIx64"/", fallback_url, current_fid);
-			}
 
 			/* According to [MS-OXOSFLD] Section 3.1.4.1, the special folder must be reused
 			   if one already exists by that name */
@@ -564,6 +560,13 @@ FolderId: 0x67ca828f02000001      Display Name: "                        ";  Con
 					OC_DEBUG(1, "Error getting new change number: %s", mapi_get_errstr(ret));
 					goto error;
 				}
+				if (!current_entry) {
+					mapistore_url = talloc_asprintf(mem_ctx, "%s0x%"PRIx64"/", fallback_url, current_fid);
+					if (!mapistore_url) {
+						OC_DEBUG(1, "Out-of-memory when allocating a MAPIStore URL");
+						goto error;
+					}
+				}
 				ret = openchangedb_create_folder(emsmdbp_ctx->oc_ctx, username, ipm_fid, current_fid, current_cn, mapistore_url, i);
 				if (ret != MAPI_E_SUCCESS) {
 					OC_DEBUG(1, "Error creating OpenChangeDB folder: %s", mapi_get_errstr(ret));
@@ -571,6 +574,13 @@ FolderId: 0x67ca828f02000001      Display Name: "                        ";  Con
 				}
 			} else if (ret == MAPI_E_SUCCESS) {
 				current_fid = found_fid;
+				if (!current_entry) {
+					mapistore_url = talloc_asprintf(mem_ctx, "%s0x%"PRIx64"/", fallback_url, current_fid);
+					if (!mapistore_url) {
+						OC_DEBUG(1, "Out-of-memory when allocating a MAPIStore URL");
+						goto error;
+					}
+				}
 				ret = openchangedb_set_mapistoreURI(emsmdbp_ctx->oc_ctx, username, current_fid, mapistore_url);
 				if (ret != MAPI_E_SUCCESS) {
 					OC_DEBUG(1, "Error setting new MAPIStore URI %s: %s", mapistore_url, mapi_get_errstr(ret));
@@ -597,6 +607,7 @@ FolderId: 0x67ca828f02000001      Display Name: "                        ";  Con
 			mapistore_del_context(emsmdbp_ctx->mstore_ctx, context_id);
 
 			if (i == EMSMDBP_INBOX) {
+				inbox_fid = current_fid;
 				/* set INBOX as receive folder for "All", "IPM", "Report.IPM" */
 				openchangedb_set_ReceiveFolder(emsmdbp_ctx->oc_ctx, username, "All", inbox_fid);
 				openchangedb_set_ReceiveFolder(emsmdbp_ctx->oc_ctx, username, "IPM", inbox_fid);
@@ -629,9 +640,6 @@ FolderId: 0x67ca828f02000001      Display Name: "                        ";  Con
 				}
 				mapistore_url = current_entry->url;
 			}
-			else {
-				mapistore_url = talloc_asprintf(mem_ctx, "%s0x%"PRIx64"/", fallback_url, current_fid);
-			}
 
 			/* According to [MS-OXOSFLD] Section 3.1.4.1, the special folder must be reused
 			   if one already exists by that name */
@@ -647,6 +655,13 @@ FolderId: 0x67ca828f02000001      Display Name: "                        ";  Con
 					OC_DEBUG(1, "Error getting new change number: %s", mapi_get_errstr(ret));
 					goto error;
 				}
+				if (!current_entry) {
+					mapistore_url = talloc_asprintf(mem_ctx, "%s0x%"PRIx64"/", fallback_url, current_fid);
+					if (!mapistore_url) {
+						OC_DEBUG(1, "Out-of-memory when allocating a MAPIStore URL");
+						goto error;
+					}
+				}
 				ret = openchangedb_create_folder(emsmdbp_ctx->oc_ctx, username, ipm_fid, current_fid, current_cn, mapistore_url, i);
 				if (ret != MAPI_E_SUCCESS) {
 					OC_DEBUG(1, "Error creating OpenChangeDB folder: %s", mapi_get_errstr(ret));
@@ -654,6 +669,13 @@ FolderId: 0x67ca828f02000001      Display Name: "                        ";  Con
 				}
 			} else if (ret == MAPI_E_SUCCESS) {
 				current_fid = found_fid;
+				if (!current_entry) {
+					mapistore_url = talloc_asprintf(mem_ctx, "%s0x%"PRIx64"/", fallback_url, current_fid);
+					if (!mapistore_url) {
+						OC_DEBUG(1, "Out-of-memory when allocating a MAPIStore URL");
+						goto error;
+					}
+				}
 				ret = openchangedb_set_mapistoreURI(emsmdbp_ctx->oc_ctx, username, current_fid, mapistore_url);
 				if (ret != MAPI_E_SUCCESS) {
 					OC_DEBUG(1, "Error setting new MAPIStore URI %s: %s", mapistore_url, mapi_get_errstr(ret));
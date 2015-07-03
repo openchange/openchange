60350ebe94296dca6b68b1d101ed419401fb20ad
Jesús García Sáez <jgarcia@zentyal.com>
2015-07-03 10:34:07 +0200

    asyncemsmdb: release full session
    mapistore_release is the talloc destructor of mstore_ctx so we should not called
    it here or we eventually will have a double free (when mstore_ctx is talloc_free)
    issue.
    
    Besides that we were not freeing sessions explicitly (when dcerpc_session was being
    freed we were having the double free crash) and we should.

--- mapiproxy/servers/default/asyncemsmdb/dcesrv_asyncemsmdb.c
+++ mapiproxy/servers/default/asyncemsmdb/dcesrv_asyncemsmdb.c
@@ -1152,8 +1152,8 @@ static NTSTATUS dcerpc_server_asyncemsmdb_unbind(struct dcesrv_connection_contex
 		OC_DEBUG(0, "[asyncemsmdb] unable to delete resolver entry %s from record %s", session->bind_addr, session->cn);
 	}
 
-	mapistore_release(session->mstore_ctx);
 	DLIST_REMOVE(asyncemsmdb_session, session);
+	talloc_free(session);
 
 	/* flush pending call on connection */
 	context->conn->pending_call_list = NULL;
